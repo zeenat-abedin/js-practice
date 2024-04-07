c672b3ff490cedb3a8b4038acdc74c7244213933
zeenat <zeenu.23@gmail.com>
2024-04-07 21:44:02 +0530

    added named arguments example in script2.js; fixed spacing in main function call

--- Day10/script2.js
+++ Day10/script2.js
@@ -9,4 +9,16 @@ function callback(d) {
     console.log(d)
 }
 
-main(2,3,callback);  
+main(2, 3, callback);  
+
+//Named arguments
+let person = {
+    name: "Zeenat",
+    role: "Developer"
+}
+
+function greet(person) {
+    console.log(person.name + " " + person.role)
+}
+
+greet(person)
