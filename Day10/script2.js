59fbb12828d035b67a4bd40b4c13b95f1cf4ad2a
zeenat <zeenu.23@gmail.com>
2024-04-08 23:11:55 +0530

    sort an array without using sort method

--- Day10/script2.js
+++ Day10/script2.js
@@ -23,4 +23,19 @@ function greet(person) {
 
 greet(person)
 
-//prototypical inheritance
+//Sort an array without using inbuilt sort method
+
+let array = [20, 5, 7, 9, 10, 45, 4]
+let temp;
+
+for(let i = 0; i < array.length; i++){
+   for(let j = i; j < array.length; j++){
+     if(array[i]>array[j]){
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+     }
+   }
+}
+
+console.log(array)
