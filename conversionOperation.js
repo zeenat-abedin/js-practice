4df136893dbe9df6c8def195895fbb47da7cc6ca
zeenat <zeenu.23@gmail.com>
2024-09-11 22:40:20 +0530

    add increment operation to gameCounter variable

--- conversionOperation.js
+++ conversionOperation.js
@@ -3,4 +3,8 @@ console.log(1 + "2") //12
 console.log("1" + 2 + 2) //122
 console.log(1 + 2 + "2") //32
 
-console.log(+true) //true
+console.log(+true) //1
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter) //101
