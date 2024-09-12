1f86aa086e3445c5eae96b08af9303085ed3ead5
zeenat <zeenu.23@gmail.com>
2024-09-12 23:08:49 +0530

    add logging of `toUpperCase()` method and original value

--- strings.js
+++ strings.js
@@ -2,3 +2,6 @@ const gameName = new String('zeenat')
 
 console.log(gameName[0])
 console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
+
+console.log(gameName) // original value remains unchanged as primitive datatype.
