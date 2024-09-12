a186680112358863551234c3060749fd8f1f7dbb
zeenat <zeenu.23@gmail.com>
2024-09-12 23:15:04 +0530

    add two new console logs: charAt(2) and indexOf('t')"

--- strings.js
+++ strings.js
@@ -3,5 +3,7 @@ const gameName = new String('zeenat')
 console.log(gameName[0])
 console.log(gameName.__proto__)
 console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
 
 console.log(gameName) // original value remains unchanged as primitive datatype.
