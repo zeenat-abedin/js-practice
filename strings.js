48cefa1fe5fa1cc7601eb3b3e2b8d45cbd4d37f5
zeenat <zeenu.23@gmail.com>
2024-09-12 23:34:42 +0530

    add new string manipulation examples

--- strings.js
+++ strings.js
@@ -10,3 +10,10 @@ console.log(gameName) // original value remains unchanged as primitive datatype.
 
 const newString = gameName.substring(0, 4)
 console.log(newString) // returns 'zeen' as a new string object.
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString) 
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne) 
+console.log(newStringOne.trim()) // returns 'hitesh'
