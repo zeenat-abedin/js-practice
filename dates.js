72b4e1105470477f34ec7c4f7b628abffba21ab3
zeenat <zeenu.23@gmail.com>
2024-09-14 22:58:08 +0530

    add toDateString method call

---
+++ dates.js
@@ -0,0 +1,7 @@
+let date = new Date()
+console.log(date.toString()) //Fri Sep 13 2024 23:45:36 GMT+0530 (India Standard Time)
+
+console.log(typeof date) //object
+
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toDateString())
