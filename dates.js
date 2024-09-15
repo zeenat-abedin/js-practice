437b5102c0ed5a4400aa02e4b17781b513b5dd69
zeenat <zeenu.23@gmail.com>
2024-09-15 22:44:07 +0530

    add new Date examples:
    timestamp, getTime, and Date.now() conversions

--- dates.js
+++ dates.js
@@ -5,3 +5,8 @@ console.log(typeof date) //object
 
 let myCreatedDate = new Date("2023-01-14")
 console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp) 
+console.log(myCreatedDate.getTime())
+console.log(Date.now()/1000)
