28c3aa5ce17db613ebf026421555842530904b52
zeenat <zeenu.23@gmail.com>
2024-09-17 21:57:32 +0530

    add function declaration and expression examples

--- scopes.js
+++ scopes.js
@@ -4,6 +4,21 @@ if (true) {
     var c = 30
 }
 
-console.log(a) //ReferenceError: a is not defined coz block scoped
-console.log(b) //ReferenceError: b is not defined coz block scoped
+// console.log(a) //ReferenceError: a is not defined coz block scoped
+// console.log(b) //ReferenceError: b is not defined coz block scoped
 console.log(c) //30 coz global scoped
+
+//function declaration gets hoisted
+console.log(addOne(5)) 
+function addOne(num) {
+    return num + 1
+}
+
+//function expression doesn't get hoisted
+
+console.log(addTwo(5)) 
+const addTwo = (num) => {
+    return num + 2
+}
+
+//ReferenceError: Cannot access 'addTwo' before initialization
