3c1098e23328687e95b69f89157afb1a6dd1d603
zeenat <zeenu.23@gmail.com>
2024-09-17 06:58:13 +0530

    add Object.freeze and Object.assign examples to objects.js

--- objects.js
+++ objects.js
@@ -10,3 +10,16 @@ Object.freeze(jsUser)
 
 jsUser.email = "john@google.com"
 console.log(jsUser)
+
+// Object.assign()
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = Object.assign({}, obj1, obj2); 
+
+console.log(obj3)
+
+//The {} is the target object. And obj1 and obj2 are the source objects.
+
+//The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object. This is of a similar concept to array flat method. So it's flat for arrays and assign for objects.
