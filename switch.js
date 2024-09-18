1335b39ff8d6605fe7b4072d6ae2878e14509ebd
zeenat <zeenu.23@gmail.com>
2024-09-18 22:01:16 +0530

    switch case examples

---
+++ switch.js
@@ -0,0 +1,19 @@
+const month = "April"
+switch (month) {
+    case "January":
+        console.log("January")
+        break;
+    case "February":
+        console.log("February")
+        break;
+    case "March": 
+        console.log("March")
+        break;
+    case "April": 
+        console.log("April")
+        break;
+    default:
+        console.log("Default case match")
+        break;
+}
+
