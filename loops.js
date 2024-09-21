7f067a16f3caf95ea49472d232b166d6665e54c1
zeenat <zeenu.23@gmail.com>
2024-09-21 23:33:23 +0530

    add for-of loop example for string iteration

--- loops.js
+++ loops.js
@@ -29,7 +29,13 @@ do {
 
 // for of
 
-const arr = [1, 2,3,4,5]
+const arr = [1, 2, 3, 4, 5]
 for (const element of arr) {
     console.log(element)
+}
+
+const greetings = 'Hello World'
+
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
 }
