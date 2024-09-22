57397b28d3723b4c2ec63f9ec8158e6b61ff9253
zeenat <zeenu.23@gmail.com>
2024-09-22 22:56:23 +0530

    add new array 'myCoding'  and run a forEach loop to log language names to console.

--- loops.js
+++ loops.js
@@ -79,3 +79,22 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 coding.forEach( (item) => {
     console.log(item)
 })
+
+const myCoding = [,
+    {
+        languageName: 'javascript',
+        languageFileName: 'js'
+    },
+    {
+        languageName: 'java',
+        languageFileName: 'java'
+    },
+    {
+        languageName: 'python',
+        languageFileName: 'py'
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName)
+})
