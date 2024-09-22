493456bd8b0f65ec8c8a28a8cb3d614fa3043121
zeenat <zeenu.23@gmail.com>
2024-09-22 23:29:16 +0530

    add map example to loops.js

--- loops.js
+++ loops.js
@@ -121,3 +121,10 @@ myNums.forEach((num) => {
 })
 
 console.log(newNumbers)
+
+// map
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNumbersMap = myNumbers.map((num) => num + 10)
+console.log(newNumbersMap)
