003acacf1853d89754df820f9e3d7b6d872fe744
zeenat <zeenu.23@gmail.com>
2024-09-24 18:41:42 +0530

    add example of Array concat method

--- arrays.js
+++ arrays.js
@@ -5,3 +5,15 @@ const arr = [1,2,3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const segregatedArr = arr.flat(1);   //you can place Infinity whenever you don't know how many nested levels you want to flatten. But it's better to place the exact value of nesting.
 
 console.log(segregatedArr);  //Output -> [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
+
+//Array concat method
+
+const arr1 = ['a', 'b', 'c']
+const arr2 = ['d', 'e', 'f']
+
+const arr3 = arr1.concat(arr2)
+
+console.log(arr3)
+
+// Output -> [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+
