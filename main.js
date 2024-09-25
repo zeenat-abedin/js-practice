d573604df49047f73220e89f0e17d4b2db71ba0c
zeenat <zeenu.23@gmail.com>
2024-09-25 11:01:25 +0530

    add new Person constructor and instances

--- main.js
+++ main.js
@@ -22,3 +22,14 @@ function sayMyName() {
     console.log(`My name is ${this.name}`)
 }
 sayMyName.call(person)
+
+// New binding
+
+function Person(name) {
+    this.name = name
+}
+
+const p1 = new Person('Vishwas')
+const p2 = new Person('Batman')
+
+console.log(p1.name,p2.name)
