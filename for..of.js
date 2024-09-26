05503f64d606849b560aa1fc1ea0db44dc5f6483
zeenat <zeenu.23@gmail.com>
2024-09-26 14:42:37 +0530

    for of examples for str and arr

---
+++ for..of.js
@@ -0,0 +1,15 @@
+// String
+
+const str = 'Vishwas'
+
+for (const char of str) {
+    console.log(char)
+}
+
+// Array
+
+const arr = ['V', 'i', 's', 'h', 'w', 'a', 's']
+
+for (const item of arr) {
+    console.log(item)
+}
