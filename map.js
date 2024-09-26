c164bfef7cb3ecfcdceffc72b34b60d80d736314
zeenat <zeenu.23@gmail.com>
2024-09-26 21:15:43 +0530

    add map example

---
+++ map.js
@@ -0,0 +1,13 @@
+const items = [
+    { name: 'Bike', price: 100 },
+    { name: 'TV', price: 200 },
+    { name: 'Album', price: 10 },
+    { name: 'Book', price: 5 },
+    { name: 'Phone', price: 500 },
+    { name: 'Computer', price: 1000 },
+    { name: 'Keyboard', price: 25 },
+]
+
+const itemNames = items.map(item => item.name)
+
+console.log(itemNames)
