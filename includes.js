b3b6f24ab79b80196374b91f494a0b26afb4c379
zeenat <zeenu.23@gmail.com>
2024-09-28 11:59:07 +0530

    add users array,
    mapping to extract user names, and a function to check if a user name exists

--- includes.js
+++ includes.js
@@ -12,3 +12,26 @@ function checkName(name) {
 
 console.log(checkName('Arabella')) // true
 console.log(checkName('Tony')) // false
+
+const users = [
+    { name: 'Arabella', age: 20 },
+    { name: 'Carina', age: 16 },
+    { name: 'Norbert', age: 35 },
+    { name: 'Laura', age: 14 }
+]
+
+const userNames = users.map(user => user.name)
+console.log(userNames)
+
+function checkUserName(userName) {
+    if (userNames.includes(userName)) {
+        console.log(`The user name: ${userName} was found`)
+    } else {
+        console.log(`The user name: ${userName} cannot be found`)
+    }
+}
+
+console.log(checkUserName('Arabella')) //The user name: Arabella was found
+console.log(checkUserName('laura')) //The user name: laura cannot be found.  
+
+//Even if Laura is there in the array but still it went into the else case as the name starts with a lowercase and therefore it is case -sensitive
