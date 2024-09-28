462ca78942630b5b7d7907a7dbf8eddfba518db0
zeenat <zeenu.23@gmail.com>
2024-09-28 11:08:50 +0530

    add new includes example

--- includes.js
+++ includes.js
@@ -3,3 +3,12 @@ const items = [1, 2, 3, 4, 5]
 const includesTwo = items.includes(2)
 
 console.log(includesTwo) //true
+
+const names = ['Arabella', 'Carina', 'Norbert', 'Laura']
+
+function checkName(name) {
+    return names.includes(name)
+}
+
+console.log(checkName('Arabella')) // true
+console.log(checkName('Tony')) // false
