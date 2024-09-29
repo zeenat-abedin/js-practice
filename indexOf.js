980e4b87d7e82f1b0cf4393196aa016d74aeaf31
zeenat <zeenu.23@gmail.com>
2024-09-29 12:32:01 +0530

    add new function `reverseCheckName` and its usage

--- indexOf.js
+++ indexOf.js
@@ -7,3 +7,10 @@ function checkName(name) {
 console.log(checkName("Arabella")) //0
 console.log(checkName("Norbert")) //2
 console.log(checkName("Tony")) //-1
+
+function reverseCheckName(name) {
+    return names.lastIndexOf(name)
+}
+
+console.log(reverseCheckName("Norbert")) //4
+console.log(reverseCheckName("Laura")) // 3
