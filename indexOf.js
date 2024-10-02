a955bf8b510c560ee898d455c870904704b4c5a1
zeenat <zeenu.23@gmail.com>
2024-10-02 22:04:24 +0530

    refactor checkName function to use arrow syntax

--- indexOf.js
+++ indexOf.js
@@ -1,8 +1,6 @@
 const names = ["Arabella", "Carina", "Norbert", "Laura", "Norbert"]
 
-function checkName(name) {
-    return names.indexOf(name)
-}
+const checkName = (name) => (names.indexOf(name))
 
 console.log(checkName("Arabella")) //0
 console.log(checkName("Norbert")) //2 
