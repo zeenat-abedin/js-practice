176f8b2b6b2590cea4da78e47e93306a5f0d5771
zeenat <zeenu.23@gmail.com>
2024-10-02 21:59:49 +0530

    add line breaks and whitespace to comments in indexOf.js

--- indexOf.js
+++ indexOf.js
@@ -5,9 +5,11 @@ function checkName(name) {
 }
 
 console.log(checkName("Arabella")) //0
-console.log(checkName("Norbert")) //2 as the indexOf() method returns the first index (position) of a specified value.
+console.log(checkName("Norbert")) //2 
+//as the indexOf() method returns the first index (position) of a specified value.
 
-console.log(checkName("Tony")) //-1 since this is not present in the array
+console.log(checkName("Tony")) //-1
+// since this is not present in the array
 
 
 function reverseCheckName(name) {
