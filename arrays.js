d9865ae22b678c0866caacd2a4e11792acfbfa29
zeenat <zeenu.23@gmail.com>
2024-10-09 21:54:00 +0530

    update console.log output for arr.flat(1) in arrays.js

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ const arr = [1,2,3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const segregatedArr = arr.flat(1);   //you can place Infinity whenever you don't know how many nested levels you want to flatten. But it's better to place the exact value of nesting.
 
-console.log(segregatedArr);  //Output -> [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
+console.log(segregatedArr);  //Output -> [1, 2, 3, 4, 5, 6, 7, 6, 7, [4, 5]]
 
 //Array concat method
 
