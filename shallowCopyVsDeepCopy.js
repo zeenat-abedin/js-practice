220b194777ba03db2a32ee9e86ce65f2cf135c34
zeenat <zeenu.23@gmail.com>
2024-10-12 15:21:33 +0530

    add example demonstrating shallow copy behavior in JavaScript arrays

--- shallowCopyVsDeepCopy.js
+++ shallowCopyVsDeepCopy.js
@@ -3,3 +3,7 @@
 let popularTeas = ['green tea', 'oolong tea', 'chai']
 let softCopyTeas = popularTeas
 console.log(softCopyTeas) // ['green tea', 'oolong tea', 'chai']
+
+popularTeas.pop()
+console.log(popularTeas) // ['green tea', 'oolong tea']
+console.log(softCopyTeas) // ['green tea', 'oolong tea']
