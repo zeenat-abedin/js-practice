362e47d3dac78d6693f2d9921b863979858adb40
zeenat <zeenu.23@gmail.com>
2024-10-12 19:17:59 +0530

    update shallow copy example and added hard copy example with array spread operator

--- shallowCopyVsDeepCopy.js
+++ shallowCopyVsDeepCopy.js
@@ -1,4 +1,4 @@
-//soft copy or shallow copy
+//soft copy
 
 let popularTeas = ['green tea', 'oolong tea', 'chai']
 let softCopyTeas = popularTeas
@@ -7,3 +7,13 @@ console.log(softCopyTeas) // ['green tea', 'oolong tea', 'chai']
 popularTeas.pop()
 console.log(popularTeas) // ['green tea', 'oolong tea']
 console.log(softCopyTeas) // ['green tea', 'oolong tea']
+
+//hard copy
+
+let topCities = ['Berlin', 'Singapore', 'New York']
+let hardCopyCities = [...topCities]
+console.log(hardCopyCities) // ['Berlin', 'Singapore', 'New York']
+
+topCities.pop()
+console.log(topCities) // ['Berlin', 'Singapore']
+console.log(hardCopyCities) // ['Berlin', 'Singapore', 'New York']
