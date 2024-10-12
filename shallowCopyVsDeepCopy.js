9491e9d1d30db14d38020115646c8b4f834a343a
zeenat <zeenu.23@gmail.com>
2024-10-12 11:15:42 +0530

    soft copy example

---
+++ shallowCopyVsDeepCopy.js
@@ -0,0 +1,5 @@
+//soft copy or shallow copy
+
+let popularTeas = ['green tea', 'oolong tea', 'chai']
+let softCopyTeas = popularTeas
+console.log(softCopyTeas) // ['green tea', 'oolong tea', 'chai']
