dd40d7a773516a99996302e17502b6ed97689268
zeenat <zeenu.23@gmail.com>
2024-10-13 22:40:42 +0530

    challenges wip

---
+++ levelUpChallenges.js
@@ -0,0 +1,17 @@
+/* 
+1. Write a for loop that loops through the array ["green tea", "black tea", "chai", "oolong tea"] and stops the loop when it finds "chai". Store all teas before "chai" in a new array named "selectedTeas"
+*/
+
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+
+for (let i = 0; i < teas.length; i++) {
+    const element = teas[i];
+    selectedTeas.push(element)
+}
+console.log(selectedTeas); // This will print: ["green tea", "black tea"]
+
+/* 
+2. Write a for loop that loops through the array ["London", "New York", "Paris", "Berlin"] and skips "Paris". Store the other cities in a new array named "visitedCities".
+*/
+
