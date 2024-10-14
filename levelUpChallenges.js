c23befc02a982524b2953634899b6f8644fa49ef
zeenat <zeenu.23@gmail.com>
2024-10-14 15:05:38 +0530

    add a for-of loop example

--- levelUpChallenges.js
+++ levelUpChallenges.js
@@ -29,5 +29,20 @@ for (let i = 0; i < cities.length; i++) {
 }
 console.log(visitedCities); // This will print: ["London", "New York", "Berlin
 
+/*
+3. Write a for-of loop that loops through the array [1, 2, 3, 4,5] and stop when the number 4 is found. Store the numbers before '4' in an array named 'smallNumbers'
+*/
 
+let numbers = [1, 2, 3, 4, 5];
+let smallNumbers = [];
 
+for (const num of numbers) {
+    if (num === 4) {
+        break;
+    }
+    smallNumbers.push(num)
+}
+
+console.log(smallNumbers)
+
+//Note - for-of is basically syntactic sugar of for loop
