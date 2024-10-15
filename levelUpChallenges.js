0bc119821dc8537f11a6653765e8fda5993fc781
zeenat <zeenu.23@gmail.com>
2024-10-15 17:00:18 +0530

    add a for-in loop to filter cities
    with population above 3 million and stores them in a new object

--- levelUpChallenges.js
+++ levelUpChallenges.js
@@ -94,3 +94,32 @@ for (const city in citiesPopulation) {
 
 console.log(cityNewPopulations)
 //Output -> { London: 8900000, 'New York': 8400000, Paris: 2140000 }
+
+/* 
+6. Use a for-in loop to loop through an object containing city populations. Skip any city with a population below 3 million and store the rest in a new object named largeCities.
+
+let worldCities = {
+ "Sydney": 5000000,
+ "Tokyo": 9000000,
+ "Berlin": 3500000,
+ "Paris": 2200000,
+}
+*/
+
+let worldCities = {
+ "Sydney": 5000000,
+ "Tokyo": 9000000,
+ "Berlin": 3500000,
+ "Paris": 2200000,
+}
+
+let largeCities = {}
+
+for (const city in worldCities) {
+    if (worldCities[city] <= 3000000) {
+       continue
+    }
+    largeCities[city] = worldCities[city]
+}
+
+console.log(largeCities)
