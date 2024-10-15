1d0e1f4b2ed41732080f103e9fcf4bc95b052af3
zeenat <zeenu.23@gmail.com>
2024-10-15 23:01:06 +0530

    add a new for-of loop example

--- levelUpChallenges.js
+++ levelUpChallenges.js
@@ -173,3 +173,21 @@ for (let i = 0; i < myNumbers.length; i++) {
 
 console.log(doubledNumbers) //Output -> [4, 10, 18]
 
+/*
+10. Use a for-of loop to iterate through the array ["chai", "green tea", "black tea", "jasmine tea", "herbal tea"] and stop when the length of the current tea name is greater than 10. Store the teas iterated over in an array named "shortTeas".
+*/
+
+let myTeas = ["chai", "green tea", "black tea", "jasmine tea", "herbal tea"]
+let shortTeas = []
+
+for (const tea of myTeas) {
+    if (tea.length > 10) {
+        break
+    }
+    shortTeas.push(tea)   
+}
+
+console.log(shortTeas) //Output-> [ 'chai', 'green tea', 'black tea' ]  
+
+
+
