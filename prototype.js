4cf24fc20d7545a9fc868277f9c4d2ee4fedfb04
zeenat <zeenu.23@gmail.com>
2024-10-17 19:04:22 +0530

    add a note about constructor function naming convention

--- prototype.js
+++ prototype.js
@@ -3,6 +3,8 @@ function Person(fName, lName) {
     this.lastName = lName
 }
 
+//Note- a constructor func. always starts with a capital letter.
+
 const person1 = new Person('Bruce', 'Wayne')
 const person2 = new Person('Clark', 'Kent')
 
