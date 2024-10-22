4e2637109a438b3df8b3dd4b7fdbecc30a812da1
zeenat <zeenu.23@gmail.com>
2024-10-22 15:54:36 +0530

    add comments explaining classes and objects in object-oriented programming

--- classes.js
+++ classes.js
@@ -16,4 +16,16 @@ let cookieTwo = new Cookie('blue')
 console.log(cookieOne) //Cookie { color: 'green' } 
 console.log(cookieTwo) //Cookie { color: 'blue' }
 
-// An object is an instance of class
+
+// Classes are blueprints for creating objects in object-oriented programming.
+
+// Classes define the properties and behaviors of objects
+
+// Objects are instances of classes
+
+// Classes can inherit properties and behaviors from other classes
+
+// Classes can have constructors to initialize objects
+
+// Classes can have methods to perform actions
+
