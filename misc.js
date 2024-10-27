6abdabb3c61539b5dfda0d9cdc76f744d04c4d8d
zeenat <zeenu.23@gmail.com>
2024-10-27 12:17:29 +0530

    add example of pass by reference with arrays

--- misc.js
+++ misc.js
@@ -10,3 +10,13 @@ function outer() {
 
 let closureFunc = outer()
 closureFunc() 
+
+//pass by reference (modifies the original array)
+
+let arr = [2, 3, 4, 5, 6]
+let get = arr
+
+get[5] = 7
+
+console.log(get)
+console.log(arr)
