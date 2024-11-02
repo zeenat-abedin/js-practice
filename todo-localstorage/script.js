a02cee16c18f619014377512ddd8a29bf8162d40
zeenat <zeenu.23@gmail.com>
2024-11-02 13:08:30 +0530

    add event listener to add-task-btn and initialized tasks array

--- todo-localstorage/script.js
+++ todo-localstorage/script.js
@@ -1,3 +1,18 @@
 const todoInput = document.getElementById("todo-input")
 const addTaskButton = document.getElementById("add-task-btn")
 const todoList = document.getElementById("todo-list")
+
+let tasks = []
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = todoInput.value.trim()
+    if (taskText === "") return
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+
+    tasks.push(newTask) 
+})
