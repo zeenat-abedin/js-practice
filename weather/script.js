3dd944cc1a7b4975559ff8a5dc95c8916bea6e35
zeenat <zeenu.23@gmail.com>
2024-11-08 21:57:09 +0530

    add four new variable declarations for HTML elements in script.js

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("city-input")
+    const cityInput = document.getElementById("city-input")
+    const getWeatherBtn = document.getElementById("city-input")
+    const weatherInfo = document.getElementById("weather-info")
+    const cityNameDisplay = document.getElementById("city-name")
 })
