19b3c42dcd4715c9d776f93b7aadf326272972da
zeenat <zeenu.23@gmail.com>
2024-11-11 16:39:52 +0530

    add className to h1 element

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return(
   <>
-    <h1>Vite with Tailwind</h1>
+    <h1 className='text-3xl bg-green-500 p-3' htmlFor=''>Vite with Tailwind</h1>
   </>
   )  
 }
