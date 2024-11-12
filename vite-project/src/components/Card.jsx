c0ca21ac17d0b966df660b92c73904daea78e75f
zeenat <zeenu.23@gmail.com>
2024-11-12 18:55:11 +0530

    add card component

---
+++ vite-project/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div>
+         <img src="" alt="" />
+          <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1> 
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quidem?</p>
+    </div>
+  )
+}
+
+export default Card
