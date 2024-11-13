276776a1a994cfc6613b7074a71366524726d300
zeenat <zeenu.23@gmail.com>
2024-11-13 22:48:01 +0530

    update img src attribute in Card component

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = () => {
   return (
     <div>
-         <img src="" alt="" />
+         <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1> 
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quidem?</p>
     </div>
