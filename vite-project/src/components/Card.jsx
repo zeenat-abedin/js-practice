13539a385323dd272be499e6e7f52a1f85657b34
zeenat <zeenu.23@gmail.com>
2024-11-14 22:43:52 +0530

    add dummy texts

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -3,7 +3,7 @@ const Card = () => {
     <div>
          <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quidem?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quidem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, nam.</p>
     </div>
   )
 }
