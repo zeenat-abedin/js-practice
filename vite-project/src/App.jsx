c856ef60c18f3c51a6d4f4e4f78dfd05c74b9658
zeenat <zeenu.23@gmail.com>
2024-11-15 22:01:30 +0530

    add Card component import and usage in App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   return(
   <>
-    <h1 className='text-3xl bg-green-500 p-3 rounded-md' htmlFor=''>Vite with Tailwind</h1>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md' htmlFor=''>Vite with Tailwind</h1>
+      <Card/>
   </>
   )  
 }
