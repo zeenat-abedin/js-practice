8d56f8eee3f03042725d329442c1a10f79420206
zeenat <zeenu.23@gmail.com>
2024-11-16 10:10:39 +0530

    update Card component with new HTML structure and styles

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,9 +1,26 @@
 const Card = () => {
   return (
     <div>
-         <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-          <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quidem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, nam.</p>
+        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
+  <div class="pt-6 text-center space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </div>
   )
 }
