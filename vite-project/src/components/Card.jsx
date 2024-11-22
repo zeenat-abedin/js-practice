67815afdfe450886f4426c7478c6d361291b1ac3
zeenat <zeenu.23@gmail.com>
2024-11-22 22:48:26 +0530

    update class attributes to use camelCase (className)

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,17 +1,17 @@
 const Card = () => {
   return (
     <div>
-       <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-       <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
-       <div class="pt-6 text-center space-y-4">
+       <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+       <img className="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
+       <div className="pt-6 text-center space-y-4">
        <blockquote>
-       <p class="text-lg font-medium">
+       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
         on large teams. It’s easy to customize, adapts to any design,
         and the build size is tiny.”
        </p>
        </blockquote>
-       <figcaption class="font-medium">
+       <figcaption className="font-medium">
        <div class="text-sky-500 dark:text-sky-400">
         Sarah Dayan
        </div>
