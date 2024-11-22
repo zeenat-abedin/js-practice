3769d4551b820d14cea7c4d67dae9955b2dec31c
zeenat <zeenu.23@gmail.com>
2024-11-22 22:44:22 +0530

    fix indentation and add trailing slashes to self-closing img and p tags in Card component

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,26 +1,26 @@
 const Card = () => {
   return (
     <div>
-        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
-  <div class="pt-6 text-center space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
+       <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+       <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
+       <div class="pt-6 text-center space-y-4">
+       <blockquote>
+       <p class="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
         on large teams. It’s easy to customize, adapts to any design,
         and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
+       </p>
+       </blockquote>
+       <figcaption class="font-medium">
+       <div class="text-sky-500 dark:text-sky-400">
         Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
+       </div>
+       <div class="text-slate-700 dark:text-slate-500">
         Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+       </div>
+       </figcaption>
+       </div>
+      </figure>    
     </div>
   )
 }
