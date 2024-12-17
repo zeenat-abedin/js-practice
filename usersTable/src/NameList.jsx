60ff4724bfb09e25a0889148be921d1469f030db
zeenat <zeenu.23@gmail.com>
2024-12-17 21:41:15 +0530

    refactor: rename 'names' array to 'persons', add 'id' property to objects,
    update variable names and keys in JSX mapping

--- usersTable/src/NameList.jsx
+++ usersTable/src/NameList.jsx
@@ -1,33 +1,38 @@
 function NameList() {
-    const names = [
+    const persons = [
         {
+            id: 1,
             "name": "John",
             "age": 30,
             "skill": "React"
         },
         {
+            id: 2,
             "name": "Bruce",
             "age": 30,
             "skill": "React"
         },
         {
+            id: 3,
             "name": "Kylie",
             "age": 30,
             "skill": "React"
         },
         {
+            id: 4,
             "name": "John",
             "age": 30,
             "skill": "Typescript"
         },
         {
+            id: 5,
             "name": "Zerab",
             "age": 30,
             "skill": "React Native"
         }
     ]
-    const namesList = names.map(name => <h2 key={names[0]}>{name}</h2>)
-    return <div>{namesList}</div>
+    const personList = persons.map(person => <h2 key={person.id}>{person.name}</h2>)
+    return <div>{personList}</div>
 }
 
 export default NameList
