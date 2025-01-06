53575e049b9bba8aee68814797c78d85c2e7c144
zeenat <zeenu.23@gmail.com>
2025-01-06 19:13:09 +0530

    add comments field to the form

--- usersTable/src/Form.jsx
+++ usersTable/src/Form.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 
 function Form() {
     const [name, setName] = useState('');
+    const [comments, setComments] = useState('')
     return (
-      <form>
-          <label>Username</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <form>
+        <div>
+            <label>Username</label>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="">Comments</label>
+            <textarea
+                value={comments}
+                onChange={(e) => setComments(e.target.value)} 
+            />
+        </div>
     </form>
   )
 }
+
 
 export default Form
