8e8f6356f5dd9161aae9b23f65b7a671a01c1c7a
zeenat <zeenu.23@gmail.com>
2025-01-22 22:35:20 +0530

    add never and void types

--- typescript-practice/variable.ts
+++ typescript-practice/variable.ts
@@ -9,8 +9,30 @@ userId.toFixed()
 //boolean 
 let isLoggedIn: boolean = false;
 
+//string
 const heroes = ["thor", "spiderman", "ironman"]
 
 heroes.map((hero): string => {
     return `hero is ${hero}`
 })
+
+//union type: more than one type
+
+function getValue(myVal: number): boolean{
+    if (myVal > 5) {
+        return true
+    }
+    return "200 OK"
+}
+
+//void
+
+function consoleError(errmsg: string): void{
+    console.error(errmsg)
+}
+
+//never
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
