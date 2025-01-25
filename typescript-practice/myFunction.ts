6d71fa2d1653b9396244df595ed04ea6192829c8
zeenat <zeenu.23@gmail.com>
2025-01-25 19:30:05 +0530

    function examples in ts

---
+++ typescript-practice/myFunction.ts
@@ -0,0 +1,23 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+addTwo(5);
+getUpper("zeenat");
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean) => {
+  if (isPaid === void 0) {
+    isPaid = false;
+  }
+};
+
+signUpUser("zeenat", "abc@gmail.com", false);
+loginUser("z", "z@gmail.com", true);
+
+export {};
