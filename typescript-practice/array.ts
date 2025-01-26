abe0564f8c0a92174108eac2d85ac8a8eb72a51e
zeenat <zeenu.23@gmail.com>
2025-01-26 16:50:29 +0530

    add heroPower array with type Array<number>

--- typescript-practice/array.ts
+++ typescript-practice/array.ts
@@ -1,5 +1,10 @@
 const superHeros: string[] = []
+// const heroPower: number[] = []
+//or you can write the above line in the below way.
+
+const heroPower: Array<number> = []
 
 superHeros.push("spiderman")
+heroPower.push(2)
 
 console.log("superHeros", superHeros);
