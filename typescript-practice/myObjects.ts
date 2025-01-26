363af8185400962acd3249482166c22b5647dc12
zeenat <zeenu.23@gmail.com>
2025-01-26 12:58:44 +0530

    add object example

---
+++ typescript-practice/myObjects.ts
@@ -0,0 +1,13 @@
+// const User = {
+//     name: 'John Doe',
+//     email: 'john@lco.dev',
+//     isActive: true,
+// }
+
+function createUser({name: string, isPaid: boolean}){
+    
+}
+
+let newUser = {name: "zeenat", isPaid: false, email: "h@h.com"}
+
+createUser(newUser)
