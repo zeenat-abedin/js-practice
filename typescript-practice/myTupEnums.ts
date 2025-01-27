4e13845d82ddece1b108a741ceeb08ca59f57f59
zeenat <zeenu.23@gmail.com>
2025-01-27 12:53:10 +0530

    add tuple destructuring and modified IUser type usage

--- typescript-practice/myTupEnums.ts
+++ typescript-practice/myTupEnums.ts
@@ -1,3 +1,8 @@
+//Tuple destructuring
+
+let tuple: [number, string, boolean] = [7, "hello", true];
+let [a, b, c] = tuple; // a: number, b: string, c: boolean
+
 let tUser: [string, number, boolean]
 
 tUser = ["jd", 131, true]
@@ -7,3 +12,5 @@ let rgb: [number, number, number] = [255, 123, 112]
 type IUser = [number, string]
 
 const newUser: IUser = [112, "example@google.com"]
+
+newUser[1] = "jd.com"
