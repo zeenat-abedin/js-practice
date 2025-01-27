fce0aa870ccba335436397ad5bf0fb44080c4852
zeenat <zeenu.23@gmail.com>
2025-01-27 10:38:49 +0530

    add type definitions and populated arrays with initial data

--- typescript-practice/array.ts
+++ typescript-practice/array.ts
@@ -4,7 +4,15 @@ const superHeros: string[] = []
 
 const heroPower: Array<number> = []
 
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
 superHeros.push("spiderman")
 heroPower.push(2)
 
-console.log("superHeros", superHeros);
+allUsers.push({name: "", isActive: true})
+
