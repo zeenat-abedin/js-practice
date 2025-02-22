b55af651deeae3f084db7e6a880ef7b15e251bac
zeenat <zeenu.23@gmail.com>
2025-02-23 01:39:44 +0530

    cleanup and check

--- reduxTookitTodo/src/App.jsx
+++ reduxTookitTodo/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>Learn about redux toolkit..</h1>
+     <h1 style={{backgroundColor: 'blanchedalmond'}}>Learn about redux toolkit..</h1>
     </>
   )
 }
