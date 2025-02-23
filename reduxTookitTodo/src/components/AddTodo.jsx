27edfea1c8b47f9540e01d08b50d516a7cea1849
zeenat <zeenu.23@gmail.com>
2025-02-23 11:39:35 +0530

    add AddTodo component

---
+++ reduxTookitTodo/src/components/AddTodo.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+
+function AddTodo() {
+    const [input, setInput] = useState('')
+
+    const addTodoHandler = () => {
+        console.log(input)
+    }
+
+  return (
+    <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
+      <input type='text' value={input} placeholder="Enter todo..." onChange={(e) => setInput(e.target.value)}/>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
+export default AddTodo
