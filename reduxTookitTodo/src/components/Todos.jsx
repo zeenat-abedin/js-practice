a819077bc72c7a2403f3908a7e3807fd7d8c01c7
zeenat <zeenu.23@gmail.com>
2025-02-23 15:18:47 +0530

    add tailwind

--- reduxTookitTodo/src/components/Todos.jsx
+++ reduxTookitTodo/src/components/Todos.jsx
@@ -8,12 +8,19 @@ function Todos() {
   return (
     <>
       <div>Todos</div>
-      {todos.map(todo => (
-        <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
-        </li>
-      ))}
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded" key={todo.id}>
+            <div className="text-white">{todo.text}</div>
+            <button
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+              onClick={() => dispatch(removeTodo(todo.id))}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
