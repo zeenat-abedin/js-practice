374e81c7c830a1d9531d5ccfc50d1d3242905ca3
zeenat <zeenu.23@gmail.com>
2025-02-23 11:41:16 +0530

    fix import statement and add semicolons

--- reduxTookitTodo/src/components/AddTodo.jsx
+++ reduxTookitTodo/src/components/AddTodo.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function AddTodo() {
-    const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
 
-    const addTodoHandler = () => {
-        console.log(input)
-    }
+  const addTodoHandler = () => {
+    console.log(input);
+  };
 
   return (
-    <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
-      <input type='text' value={input} placeholder="Enter todo..." onChange={(e) => setInput(e.target.value)}/>
-      <button type='submit'>Submit</button>
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        value={input}
+        placeholder="Enter todo..."
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
-export default AddTodo
+export default AddTodo;
