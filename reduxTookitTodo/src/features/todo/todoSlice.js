e4af3e7c6efed981d748eb52046cd6befb306302
zeenat <zeenu.23@gmail.com>
2025-02-23 10:17:54 +0530

    `feat: updated todoSlice.js with initial state`

--- reduxTookitTodo/src/features/todo/todoSlice.js
+++ reduxTookitTodo/src/features/todo/todoSlice.js
@@ -1,1 +1,15 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+
+const initialState = {
+    todos: [{id: 1, text: 'Hello world'}]
+}
+    
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: () => {},
+        removeTodo: () => {}
+    }
+})
