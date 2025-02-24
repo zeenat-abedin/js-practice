3d85935ba7913ab617dc86e6fc4b35026ecc6422
zeenat <zeenu.23@gmail.com>
2025-02-24 10:59:44 +0530

    add tailwind

--- reduxTookitTodo/src/App.jsx
+++ reduxTookitTodo/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-     <h1 style={{backgroundColor: 'blanchedalmond'}}>Learn about redux toolkit..</h1>
+      <h1 style={{ backgroundColor: "blanchedalmond" }}>
+        Learn about redux toolkit..
+      </h1>
+      <AddTodo/>
+      <Todos/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
