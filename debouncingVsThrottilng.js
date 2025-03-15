d589f48bc4d71cb70ec38bacf0b0f073083039fd
zeenat <zeenu.23@gmail.com>
2025-03-15 12:17:16 +0530

    debounce vs throttling

---
+++ debouncingVsThrottilng.js
@@ -0,0 +1,6 @@
+// debouncing vs throttling
+
+// Create a button UI and add debounce as follows 
+// -->show "Button pressed <X> times" everytime button is pressed 
+
+// --> Increase "Trigerred <Y> times" count after 800ms of debounce
