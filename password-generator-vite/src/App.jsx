f395bd2f9461734a490eb98921d4e0e91a55c7d6
zeenat <zeenu.23@gmail.com>
2025-03-22 15:11:00 +0530

    add more classes

--- password-generator-vite/src/App.jsx
+++ password-generator-vite/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md'>
-     <h1 className='text-white text-center bg-red-400'>Passoword Generator</h1>
+     <h1 className='text-white text-center bg-red-400'>Password Generator</h1>
     </div>
   )
 }
