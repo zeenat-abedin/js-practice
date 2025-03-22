894e4833bf6eaa8d40af402a6ee4a841645255f0
zeenat <zeenu.23@gmail.com>
2025-03-22 15:47:57 +0530

    add input field and button

--- password-generator-vite/src/App.jsx
+++ password-generator-vite/src/App.jsx
@@ -8,8 +8,18 @@ function App() {
   const [ password, setPassword ] = useState('')
 
   return (
-    <div className='w-full max-w-md mx-auto shadow-md'>
-     <h1 className='text-white text-center bg-red-400'>Password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md  bg-gray-800 text-orange-500'>
+     <h1 className='text-white text-center'>Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+      </div>
     </div>
   )
 }
