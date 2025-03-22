fecb57ecf2fb6945158582ec46b3c05989170203
zeenat <zeenu.23@gmail.com>
2025-03-22 15:58:23 +0530

    add input range

--- password-generator-vite/src/App.jsx
+++ password-generator-vite/src/App.jsx
@@ -1,27 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [ numberAllowed, setNumberAllowed ] = useState(false)
-  const [charAllowed, setCharAllowed] = useState(false)
-  const [ password, setPassword ] = useState('')
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
   return (
-    <div className='w-full max-w-md mx-auto shadow-md  bg-gray-800 text-orange-500'>
-     <h1 className='text-white text-center'>Password Generator</h1>
+    <div className="w-full max-w-md mx-auto shadow-md  bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center">Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
           type="text"
           value={password}
-          className='outline-none w-full py-1 px-3'
-          placeholder='Password'
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
           readOnly
         />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <input 
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className="cursor-pointer"
+          name=""
+          id="" 
+      />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
