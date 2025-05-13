8f7d91c1566bc78d55fcc9bf1a7f1c77a1b3c224
zeenat <zeenu.23@gmail.com>
2025-05-14 02:53:48 +0530

    add ex of pass by reference and pass by value

--- Arrays/practice.js
+++ Arrays/practice.js
@@ -10,3 +10,23 @@ function outer(){
 
 let closureFunc = outer()
 closureFunc()
+
+//pass by reference (modifies the original array)
+
+let arr = [1, 2, 3, 4, 5]
+let get = arr
+
+get[5] = 6
+
+console.log(get)
+console.log(arr);
+
+//pass by value (doesn't modify the original arr)
+
+let arr2 = [6, 7, 8, 9, 5]
+let set = [...arr2]
+
+set[5] = 6
+
+console.log(set)
+console.log(arr2);
