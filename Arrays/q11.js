762457d36c8ae154255e884a3ed94c6332c04637
zeenat <zeenu.23@gmail.com>
2025-05-20 02:20:18 +0530

    higher order func example

--- Arrays/q11.js
+++ Arrays/q11.js
@@ -3,5 +3,15 @@ const numbers = [1,2,3,4,5]
 const double = numbers.map(num => num * 2)
 
 console.log(double);
-console.log(numbers);
+console.log(numbers); //doesn't modify the original array
+
+//higher order function example
+
+const numArr = [2,4,6,8,10,12,14]
+
+const half = numArr.map(function (num) {
+    return num / 2
+})
+
+console.log(half);
 
