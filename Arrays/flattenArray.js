534e9640a5b2f7555fa2887164be12385df735ff
zeenat <zeenu.23@gmail.com>
2025-06-10 10:58:16 +0530

    add recursive and iterative approach

--- Arrays/flattenArray.js
+++ Arrays/flattenArray.js
@@ -4,8 +4,49 @@
 // The problem involves transforming an array that contains nested arrays of various levels into a single-level array. For example, given an input like [[1, 2, [3]], 4].
 // The expected output would be [1, 2, 3, 4]
 
+// Recurive Solution
 function flattenArray(value) {
-    
-}
+    // Initialize an empty array to hold the flattened result
+    let results = [];
   
-export default flattenArray;
+    // Check if the input value is an array
+    if (Array.isArray(value)) {
+      // If value is an array, iterate over each element
+      value.forEach(val => {
+        // Recursively flatten the element and concatenate it with the results array
+        results.push(...flattenArray(val));
+      });
+    } else {
+      // If value is not an array, add it directly to the results array
+      results.push(value);
+    }
+  
+    // Return the flattened results array
+    return results;
+  }
+  
+  // Iterative solution
+  function flattenArray(value) {
+    // Initialize an empty array to hold the flattened result
+    let results = [];
+    // Initialize a stack with the input array to manage the iteration
+    let stack = [...value];
+  
+    // Loop until the stack is empty
+    while (stack.length) {
+      // Remove and return the last element from the stack
+      let current = stack.pop();
+      // Check if the current element is an array
+      if (Array.isArray(current)) {
+        // If current is an array, add its elements to the end of the stack
+        current.forEach(val => stack.push(val));
+      } else {
+        // If current is not an array, add it to the beginning of the results array
+        results.unshift(current);
+      }
+    }
+  
+    // Return the flattened results array
+    return results;
+  }
+  
