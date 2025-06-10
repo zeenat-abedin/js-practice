3d430af4d32f4a2b103f72806989797c1be3a969
zeenat <zeenu.23@gmail.com>
2025-06-10 10:33:58 +0530

    flattenArray func. declaration

---
+++ Arrays/flattenArray.js
@@ -0,0 +1,11 @@
+// Write a function to flatten a multi-dimensional array into a single-level array.
+// Provide both recursive and iterative solutions.
+
+// The problem involves transforming an array that contains nested arrays of various levels into a single-level array. For example, given an input like [[1, 2, [3]], 4].
+// The expected output would be [1, 2, 3, 4]
+
+function flattenArray(value) {
+    
+}
+  
+export default flattenArray;
