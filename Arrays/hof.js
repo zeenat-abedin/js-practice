9bc3756ba7d9a14ee66b2aef34db45c26470ad8f
zeenat <zeenu.23@gmail.com>
2025-06-15 20:28:49 +0530

    add comments to hof.js explaining the use of map, filter, and reduce methods

--- Arrays/hof.js
+++ Arrays/hof.js
@@ -32,3 +32,8 @@ console.log(isOdd);
 
 const isEven = arr.filter(x => x % 2 === 0 )
 console.log(isEven);
+
+// 1. map method is used when we want transformation of whole array.
+// 2. filter is used when we want to filter the array to obtain required value.
+// 3. reduce is used when we want to reduce the array to single value eg (max, min, avg, sum, difference etc).
+// 4. reduce passes two arguments one function(which includes accumulator and initial value as argument itself) and another initial value of accumulator.
