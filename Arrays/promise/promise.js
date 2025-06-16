67d89a2ba3c7a656459d1fd577495887b552fa52
zeenat <zeenu.23@gmail.com>
2025-06-16 15:11:34 +0530

    refactor code to use promise chaining instead of callbacks

--- Arrays/promise/promise.js
+++ Arrays/promise/promise.js
@@ -6,3 +6,19 @@ console.log(user);
 
 user.then((data)=> console.log(data));
 
+// promise over callback
+
+const cart = ['tshirt', 'jeans', 'shoes']
+
+createOrder(cart, function (orderId){
+    proceedToPayment(orderId, function (paymentInfo){
+        orderSummary(paymentInfo, function () {
+            updateWalletBalance()
+        });
+    });
+});
+
+createOrder(cart)
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo) => orderSummary(paymentInfo))
+  .then((paymentInfo) => updateWalletBalance(paymentInfo))
