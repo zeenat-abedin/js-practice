b32e8642b3e9fbdc702a74316928f2e7ba7774ce
zeenat <zeenu.23@gmail.com>
2025-06-17 13:30:00 +0530

    add Promise.allSettled to promiseAPIs.js

--- Arrays/promise/promiseAPIs.js
+++ Arrays/promise/promiseAPIs.js
@@ -3,16 +3,24 @@ const p1 = new Promise((resolve, reject) => {
 });
 
 const p2 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("p2 success"), 1000);
-  setTimeout(() => reject("p2 fail"), 1000);
+  setTimeout(() => resolve("p2 success"), 1000);
 });
 
 const p3 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("p3 success"), 2000);
+//   setTimeout(() => resolve("p3 success"), 2000);
+  setTimeout(() => reject("p3 fail"), 2000);
 });
 
 
 Promise.all([p1, p2, p3])
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
+Promise.allSettled([p1, p2, p3])
     .then((res) => {
         console.log(res);
     })
