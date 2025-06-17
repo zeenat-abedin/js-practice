c531d2153011feea3371aed71aaeaa790bc7505d
zeenat <zeenu.23@gmail.com>
2025-06-17 21:24:57 +0530

    add recursive solution to flatten multi-dimensional array

--- Arrays/flattenArray.js
+++ Arrays/flattenArray.js
@@ -1,7 +1,8 @@
 // Write a function to flatten a multi-dimensional array into a single-level array.
 // Provide both recursive and iterative solutions.
 
-// The problem involves transforming an array that contains nested arrays of various levels into a single-level array. For example, given an input like [[1, 2, [3]], 4].
+// The problem involves transforming an array that contains nested arrays of various levels into a single-level array.
+// For example, given an input like [[1, 2, [3]], 4].
 // The expected output would be [1, 2, 3, 4]
 
 // Recurive Solution
