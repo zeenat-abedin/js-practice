26cc765e9616664cea0fd842fb63df38b187ec2e
zeenat <zeenu.23@gmail.com>
2025-06-17 15:42:49 +0530

    modify timeout for p2, add allSettled and race handlers

--- Arrays/promise/promiseAPIs.js
+++ Arrays/promise/promiseAPIs.js
@@ -3,7 +3,7 @@ const p1 = new Promise((resolve, reject) => {
 });
 
 const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("p2 success"), 1000);
+  setTimeout(() => resolve("p2 success"), 5000);
 });
 
 const p3 = new Promise((resolve, reject) => {
@@ -21,6 +21,14 @@ Promise.all([p1, p2, p3])
     })
 
 Promise.allSettled([p1, p2, p3])
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
+Promise.race([p1, p2, p3])
     .then((res) => {
         console.log(res);
     })
