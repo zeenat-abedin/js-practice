d10d352ee25ce4c06c0359bdfe9b6abe774e4a5e
zeenat-abedin <zeenu.23@gmail.com>
2025-06-27 21:00:39 +0530

    update App.js

--- Arrays/inception/App.js
+++ Arrays/inception/App.js
@@ -9,21 +9,35 @@ const Header = () => {
           src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
           alt="logo-img"
         />
-      </div>.
+      </div>
       <div className="nav-items">
         <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
         </ul>
       </div>
     </div>
   );
 };
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"></div>
+      <div className="res-container"></div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
