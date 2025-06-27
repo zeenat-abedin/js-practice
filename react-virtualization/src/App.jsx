04454e5168a192302bd00c7042d8906e6ee3071f
zeenat-abedin <zeenu.23@gmail.com>
2025-06-27 15:19:42 +0530

    format file

--- react-virtualization/src/App.jsx
+++ react-virtualization/src/App.jsx
@@ -1,5 +1,5 @@
-import {useState, useRef } from 'react';
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
 
 const ROW_HEIGHT = 30; // each row’s height in px
 const VISIBLE_ROWS = 10;
@@ -7,52 +7,56 @@ const VISIBLE_ROWS = 10;
 function App() {
   const array = [];
   for (let i = 0; i < 100; i++) {
-    array.push({name: "Swiggy", value: i})
+    array.push({ name: "Swiggy", value: i });
   }
 
   const containerRef = useRef(null);
-  const [startIndex, setStartIndex ] = useState(0)
-  const totalHeight = array.length * ROW_HEIGHT
+  const [startIndex, setStartIndex] = useState(0);
+  const totalHeight = array.length * ROW_HEIGHT;
 
   const handleScroll = () => {
-    const scrollTop = containerRef.current.scrollTop
-    const newStartIndex = Math.floor(scrollTop / ROW_HEIGHT)
-    setStartIndex(newStartIndex)
-  }
+    const scrollTop = containerRef.current.scrollTop;
+    const newStartIndex = Math.floor(scrollTop / ROW_HEIGHT);
+    setStartIndex(newStartIndex);
+  };
 
   const visibleData = array.slice(startIndex, startIndex + VISIBLE_ROWS);
- 
+
   return (
-    <div 
+    <div
       className="App"
       ref={containerRef}
       onScroll={handleScroll}
       style={{
-        overflowY: 'auto',
+        overflowY: "auto",
         height: VISIBLE_ROWS * ROW_HEIGHT,
-        position: 'relative'
+        position: "relative",
       }}
-     >
-      <div style={{ height: totalHeight , position: 'relative'}}>
-        <table style={{ position: 'absolute', top: startIndex * ROW_HEIGHT }}>
+    >
+      <div style={{ height: totalHeight, position: "relative" }}>
+        <table style={{ position: "absolute", top: startIndex * ROW_HEIGHT }}>
           <thead>
             <tr>
-              <td><strong>Name</strong></td>
-              <td><strong>Value</strong></td>
+              <td>
+                <strong>Name</strong>
+              </td>
+              <td>
+                <strong>Value</strong>
+              </td>
             </tr>
           </thead>
           <tbody>
-            {visibleData.map((item, index) => 
+            {visibleData.map((item, index) => (
               <tr key={startIndex + index} style={{ height: ROW_HEIGHT }}>
                 <td>{item.name}</td>
                 <td>{item.value}</td>
               </tr>
-            )}
+            ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
