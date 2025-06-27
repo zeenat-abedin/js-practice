a35bcdbc4ee764baa95cbd9ecd057d0f59fbe92f
zeenat-abedin <zeenu.23@gmail.com>
2025-06-27 18:35:19 +0530

    build(deps): update App.js, package-lock.json and package.json

--- Arrays/inception/App.js
+++ Arrays/inception/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Namaste React"
-)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
+          alt="logo-img"
+        />
+      </div>.
+      <div className="nav-items">
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+      </div>
+    </div>
+  );
+};
 
-root.render(heading)
+const AppLayout = () => {
+  return <div className="app"></div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(AppLayout);
