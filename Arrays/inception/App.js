ae08ff8fc7fd787ba2b9d061ea89ad4af6c46e95
zeenat-abedin <zeenu.23@gmail.com>
2025-06-27 21:59:18 +0530

    update App.js and index.css

--- Arrays/inception/App.js
+++ Arrays/inception/App.js
@@ -27,6 +27,8 @@ const RestaurantCard = () => {
         <div className="res-card" style={{backgroundColor: '#f0f0f0'}}>
             <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/6/6/f6c81999-57b6-4eb7-ac66-dc1c3485f2e4_6ba607a5-1751-462a-a73d-68ae91ade469.jpg" alt="res-logo" />
             <h3>Meghana Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.3 stars</h4>
         </div>
     )
 }
