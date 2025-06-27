dfb6f21df288d1a2a3a2e98dd767b2525279dcae
zeenat-abedin <zeenu.23@gmail.com>
2025-06-27 19:15:00 +0530

    build(deps): update App.js, package-lock.json and package.json

--- Arrays/inception/App.js
+++ Arrays/inception/App.js
@@ -11,7 +11,12 @@ const Header = () => {
         />
       </div>.
       <div className="nav-items">
-
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
