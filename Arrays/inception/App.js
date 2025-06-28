68f480b9704cd97641960e51916474cd582955b4
zeenat-abedin <zeenu.23@gmail.com>
2025-06-28 12:12:02 +0530

    refactor: move Header component to its own file

--- Arrays/inception/App.js
+++ Arrays/inception/App.js
@@ -1,26 +1,5 @@
 import ReactDOM from "react-dom/client";
-
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
-          alt="logo-img"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+import Header from "./src/components/Header";
 
 const RestaurantCard = (props) => {
     const { resData } = props;
---
+++ Arrays/inception/src/components/Header.js
@@ -0,0 +1,25 @@
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
+          alt="logo-img"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+export default Header;
