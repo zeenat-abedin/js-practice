f7eb30312166f7db19e1c559144f41172e94debb
zeenat-abedin <zeenu.23@gmail.com>
2025-06-28 13:44:52 +0530

    feat: add filter button for top rated restaurants in Body component

--- Arrays/inception/src/components/Body.js
+++ Arrays/inception/src/components/Body.js
@@ -4,7 +4,9 @@ import resList from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {}}>Top Rated Restaurants</button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
