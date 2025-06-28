5667f5b01d14702b6be1ff91e899c85f6cd84d47
zeenat-abedin <zeenu.23@gmail.com>
2025-06-28 12:29:50 +0530

    feat: add mock data and constants for restaurant listings

--- Arrays/inception/src/components/Body.js
+++ Arrays/inception/src/components/Body.js
@@ -1,4 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
 
 const Body = () => {
   return (
---
+++ Arrays/inception/src/utils/mockData.js
@@ -0,0 +1,90 @@
+const resList = [
+  {
+    type: "restaurant",
+    data: {
+      id: "178387",
+      name: "Hyderabadi Handi Biryani",
+      city: "1",
+      slugs: {
+        restaurant: "hyderabadi-handi-biryani-marathahalli-marathahalli",
+      },
+      cloudinaryImageId: "ewzw2m8whkincsto9kqm",
+      locality: "CKB Layout",
+      areaName: "Marathahalli",
+      costForTwo: "₹400 for two",
+      cuisines: [
+        "Biryani",
+        "North Indian",
+        "Desserts",
+        "Tandoor",
+        "Hyderabadi",
+        "Andhra",
+      ],
+      avgRating: 4,
+      feeDetails: {
+        restaurantId: "178387",
+        title: "Delivery Charge",
+        amount: "0",
+      },
+      totalRatingsString: "23K+",
+      promoted: true,
+      availability: {
+        nextCloseTime: "2025-06-28 04:45:00",
+        opened: true,
+      },
+      badges: {},
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+        logoCtx: {
+          text: "BENEFITS",
+        },
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      id: "178387",
+      name: "KFC Burgers",
+      city: "1",
+      slugs: {
+        restaurant: "hyderabadi-handi-biryani-marathahalli-marathahalli",
+      },
+      cloudinaryImageId: "ewzw2m8whkincsto9kqm",
+      locality: "CKB Layout",
+      areaName: "Marathahalli",
+      costForTwo: "₹400 for two",
+      cuisines: [
+        "Biryani",
+        "North Indian",
+        "Desserts",
+        "Tandoor",
+        "Hyderabadi",
+        "Andhra",
+      ],
+      avgRating: 4,
+      feeDetails: {
+        restaurantId: "178387",
+        title: "Delivery Charge",
+        amount: "0",
+      },
+      totalRatingsString: "23K+",
+      promoted: true,
+      availability: {
+        nextCloseTime: "2025-06-28 04:45:00",
+        opened: true,
+      },
+      badges: {},
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+        logoCtx: {
+          text: "BENEFITS",
+        },
+      },
+    },
+  },
+];
+
+export default resList;
