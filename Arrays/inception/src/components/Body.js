51ffd1af48f50cbddbaa7a7844bf13a58527c649
zeenat-abedin <zeenu.23@gmail.com>
2025-07-01 20:53:23 +0530

    feat: add click handler to filter button in Body component

--- Arrays/inception/src/components/Body.js
+++ Arrays/inception/src/components/Body.js
@@ -5,7 +5,10 @@ const Body = () => {
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {}}>Top Rated Restaurants</button>
+        <button className="filter-btn"
+         onClick={() => {
+          console.log("Filter button clicked");
+         }}>Top Rated Restaurants</button>
       </div>
       <div className="res-container">
         {resList.map((restaurant) => (
