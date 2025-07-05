44239d6259cda0f2bf4d3365727a02a9e23d793f
zeenat-abedin <zeenu.23@gmail.com>
2025-07-05 23:49:57 +0530

    refactor: update package.json and package-lock.json to include react-router-dom and cookie dependencies; modify fetchData URL to use CORS proxy; enhance Shimmer component structure

--- Arrays/namaste-react/src/components/Body.jsx
+++ Arrays/namaste-react/src/components/Body.jsx
@@ -20,7 +20,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9304278&lng=77.678404&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9304278&lng=77.678404&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     
--- Arrays/namaste-react/src/components/Shimmer.jsx
+++ Arrays/namaste-react/src/components/Shimmer.jsx
@@ -1,13 +1,26 @@
-import "../index.css";
-
-function Shimmer() {
+const Shimmer = () => {
   return (
-    <div className="shimmer">
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Shimmer
+export default Shimmer;
