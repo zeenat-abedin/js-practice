258c69d22dab39ea6a447eee4ca76f8211937395
zeenat-abedin <zeenu.23@gmail.com>
2025-07-05 08:50:35 +0530

    refactor: update fetchData to use new API endpoint and improve restaurant data handling

--- Arrays/namaste-react/src/components/Body.jsx
+++ Arrays/namaste-react/src/components/Body.jsx
@@ -3,35 +3,62 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 
+// API URL
+//"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9536392&lng=77.695126&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   if (listOfRestaurants.length === 0) {
     return <Shimmer />;
   }
 
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+
   useEffect(() => {
     fetchData();
   }, [])
 
   const fetchData = async () => {
-    const res = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9536392&lng=77.695126&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    const apiData = await res.json();
-    setListOfRestaurants(apiData.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setListOfRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
   return (
     <div className="body">
       <div className="filter">
         <div className="search">
-          <input 
+          <input
             type="text"
-            value={searchText} 
-            onChange={(e) => setSearchText(e.target.value)} 
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
             className="search-box"
           />
-          <button onClick={() => console.log(searchText)}>Search</button>
+          <button
+            onClick={() => {
+              // Filter the restaurant cards and update the UI
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+
         </div>
         <button
           className="filter-btn"
