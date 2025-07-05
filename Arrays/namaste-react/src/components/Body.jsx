b7469e0b5786cbb026b9eab304388ec51718c8a1
zeenat-abedin <zeenu.23@gmail.com>
2025-07-05 10:26:37 +0530

    refactor: update fetchData to use the correct index for restaurant data retrieval

--- Arrays/namaste-react/src/components/Body.jsx
+++ Arrays/namaste-react/src/components/Body.jsx
@@ -24,12 +24,13 @@ const Body = () => {
     );
     const json = await data.json();
     
+    console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     setListOfRestaurants(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     setFilteredRestaurant(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
