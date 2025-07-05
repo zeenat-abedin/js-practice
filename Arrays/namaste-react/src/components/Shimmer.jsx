66f16d735551da49d9810d3ecfc1197cbcfba44c
zeenat-abedin <zeenu.23@gmail.com>
2025-07-05 09:04:26 +0530

    refactor: update Shimmer component to remove placeholder text and adjust shimmer-card background color

--- Arrays/namaste-react/src/components/Shimmer.jsx
+++ Arrays/namaste-react/src/components/Shimmer.jsx
@@ -3,9 +3,9 @@ import "../index.css";
 function Shimmer() {
   return (
     <div className="shimmer">
-      <div className="shimmer-card">Cards</div>
-      <div className="shimmer-card">Cards</div>
-      <div className="shimmer-card">Cards</div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
     </div>
   )
 }
