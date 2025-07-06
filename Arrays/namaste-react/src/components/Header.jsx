24230ae3333b9de497807b1e5745bf76d630e411
zeenat-abedin <zeenu.23@gmail.com>
2025-07-06 22:30:25 +0530

    refactor: update Header component to use Link for navigation

--- Arrays/namaste-react/src/components/Header.jsx
+++ Arrays/namaste-react/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -14,16 +15,24 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-          <button 
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <button
             className="login-btn"
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
-            >
+          >
             {btnName}
           </button>
         </ul>
