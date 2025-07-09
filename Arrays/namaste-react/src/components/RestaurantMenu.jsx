b1f95f9ca7970fe6a1752c5dfccf5acae13844af
zeenat-abedin <zeenu.23@gmail.com>
2025-07-09 09:19:18 +0530

    feat: implement menu fetching logic in RestaurantMenu component

--- Arrays/namaste-react/src/components/RestaurantMenu.jsx
+++ Arrays/namaste-react/src/components/RestaurantMenu.jsx
@@ -1,15 +1,30 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 function RestaurantMenu() {
-  return (
-    <div>
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
-      <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
-      </ul>
-    </div>
-  )
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+      fetchMenu();
+    }, [])
+
+    const fetchMenu = async () => {
+        const res = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=");
+        const data = await res.json();
+        console.log(data);
+    }
+    if (resInfo === null) return <Shimmer />;
+    return (
+        <div>
+            <h1>Name of the Restaurant</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Burgers</li>
+                <li>Diet Coke</li>
+            </ul>
+        </div>
+    )
 }
 
 export default RestaurantMenu
