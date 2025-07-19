eafe4e7945214e46734b1ce7bc90ffd5a93ae9f0
zeenat-abedin <zeenu.23@gmail.com>
2025-07-19 19:17:01 +0530

    feat: add User component to display user information

---
+++ Arrays/namaste-react/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      <h2>Name: Zeenat</h2>
+      <h3>Location: Bengaluru</h3>
+      <h4>Contact: zeenu.23@gmail.com</h4>
+    </div>
+  )
+}
+
+export default User
