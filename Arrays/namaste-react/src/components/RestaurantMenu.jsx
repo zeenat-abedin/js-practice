ebb02d4b7ea443a00a740028b16a44b89e96c5e7
zeenat-abedin <zeenu.23@gmail.com>
2025-07-20 23:49:59 +0530

    refactor: restructure RestaurantMenu component for improved readability and functionality

--- Arrays/namaste-react/src/components/RestaurantMenu.jsx
+++ Arrays/namaste-react/src/components/RestaurantMenu.jsx
@@ -1,37 +1,51 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
-function RestaurantMenu() {
-    const [resInfo, setResInfo] = useState(null);
+const RestaurantMenu = () => {
+  const { resId } = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    }, [])
+  const dummy = "Dummy Data";
 
-    const fetchMenu = async () => {
-        const res = await fetch("/api?id=221738");
-        const json = await res.json();
-        console.log(json);
-        setResInfo(json.data);
-    }
+  const resInfo = useRestaurantMenu(resId);
 
-    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info || {};
+  const [showIndex, setShowIndex] = useState(null);
 
-    if (resInfo === null) return <Shimmer />;
-    
-    return (
-        <div className="menu">
-            <h1>{name}</h1>
-            <p>{cuisines.join(", ")} - {costForTwoMessage}</p>
-            <h2>Menu</h2>
-            <ul>
-                <li>Biryani</li>
-                <li>Burgers</li>
-                <li>Diet Coke</li>
-                <li>Paneer Tikka</li>
-            </ul>
-        </div>
-    )
-}
+  if (resInfo === null) return <Shimmer />;
 
-export default RestaurantMenu
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.["card"]?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  //console.log(categories);
+
+  return (
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      {/* categories accordions */}
+      {categories.map((category, index) => (
+        // controlled component
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index)}
+          dummy={dummy}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantMenu;
