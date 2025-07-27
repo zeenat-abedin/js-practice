e69946eb5b51a4d7b9456783ba39955dcf3dbade
zeenat-abedin <zeenu.23@gmail.com>
2025-07-27 09:46:19 +0530

    feat: display online status indicator in Header component

--- Arrays/namaste-react/src/components/Header.jsx
+++ Arrays/namaste-react/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +17,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>{onlineStatus ? "✅": "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
