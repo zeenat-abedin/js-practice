6f75f7f936c05added58c4bfb6c54455472055df
zeenat-abedin <zeenu.23@gmail.com>
2025-07-27 08:39:01 +0530

    refactor: update useRestaurantMenu hook to manage state and return restaurant info

--- Arrays/namaste-react/src/utils/useRestaurantMenu.js
+++ Arrays/namaste-react/src/utils/useRestaurantMenu.js
@@ -1,16 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MENU_API } from "./constants";
 
 const useRestaurantMenu = (resId) => {
+    const [resInfo, setResInfo] = useState(null);
     //fetch restaurant menu data
     useEffect(()=>{
         fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchData = async () => {
         const res = await fetch(MENU_API + resId)
         const data = await res.json();
-        return data;
+        setResInfo(data);
     }
 
     return resInfo;
