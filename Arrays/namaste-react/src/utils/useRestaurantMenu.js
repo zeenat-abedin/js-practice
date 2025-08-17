eb2a05bcddd58b636cbc5e1f736610ed494ef275
zeenat-abedin <zeenu.23@gmail.com>
2025-08-17 22:14:24 +0530

    fix formatting and indentation in useRestaurantMenu hook

--- Arrays/namaste-react/src/utils/useRestaurantMenu.js
+++ Arrays/namaste-react/src/utils/useRestaurantMenu.js
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import { MENU_API } from "./constants";
 
 const useRestaurantMenu = (resId) => {
-    const [resInfo, setResInfo] = useState(null);
-    
-    useEffect(()=>{
-        fetchData();
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  }, []);
 
-    const fetchData = async () => {
-        const res = await fetch(MENU_API + resId)
-        const json = await res.json();
-        setResInfo(json.data);
-    }
+  const fetchData = async () => {
+    const res = await fetch(MENU_API + resId);
+    const json = await res.json();
+    setResInfo(json.data);
+  };
 
-    return resInfo;
-}
+  return resInfo;
+};
 
 export default useRestaurantMenu;
