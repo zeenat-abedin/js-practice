a4dd71caea769813fb7142c8ee0cdd441406f97a
zeenat-abedin <zeenu.23@gmail.com>
2025-08-30 12:38:57 +0530

    add func for searching an element

---
+++ Arrays/searchELement.js
@@ -0,0 +1,12 @@
+// Write a function that searches for an element in an array and returns the index. If the element is not present then just return -1.
+
+function searchElement(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(searchElement([4, 2, 0, 10, 8, 30], 8));
