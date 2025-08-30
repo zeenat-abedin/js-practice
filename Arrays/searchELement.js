715c3de89e3d034b54fe7d7d4fd12637f45d8d90
zeenat-abedin <zeenu.23@gmail.com>
2025-08-30 13:11:39 +0530

    add countNegatives func.

--- Arrays/searchELement.js
+++ Arrays/searchELement.js
@@ -10,3 +10,17 @@ function searchElement(arr, target) {
 }
 
 console.log(searchElement([4, 2, 0, 10, 8, 30], 8));
+
+// Write a function that returns the number of negative numbers in an array
+
+function countNegatives(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+  return count
+}
+
+console.log(countNegatives([2, -9, 17, 0, 1, -10, -4, 8]));
