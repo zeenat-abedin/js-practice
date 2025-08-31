f5b8f791094d56c76fbbcc4c1efd511cc06011c7
zeenat-abedin <zeenu.23@gmail.com>
2025-08-31 11:07:36 +0530

    findLargestNum with all negative values example

--- Arrays/largestNum.js
+++ Arrays/largestNum.js
@@ -11,3 +11,17 @@ function findLargest(arr) {
 }
 
 console.log(findLargest([5, 0, 10, 8, 17]));
+
+//another example with all negative values for the above
+
+function findLargestNum(arr) {
+  let largest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest
+}
+
+console.log(findLargestNum([-9, -19, -3]));
