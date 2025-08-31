b8f2b1e1b32aed2b9800f99a4ec1ba88f5f7c50f
zeenat-abedin <zeenu.23@gmail.com>
2025-08-31 14:27:02 +0530

    findSecondLargest in an array

---
+++ Arrays/findSecondLargest.js
@@ -0,0 +1,16 @@
+function findSecondLargest(arr) {
+    let firstLargest = -Infinity;
+    let secondLargest = -Infinity
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = arr[i]
+        } else if(arr[i] > secondLargest){
+           secondLargest = arr[i]
+        }
+    }
+    return secondLargest;
+}
+
+
+console.log(findSecondLargest([4, 9, 0, 2, 8, 7, 1, 10]))
