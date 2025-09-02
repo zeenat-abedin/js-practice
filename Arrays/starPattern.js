5589b582a4afd18b9e7a445acc3bd48cb07c5498
zeenat-abedin <zeenu.23@gmail.com>
2025-09-02 20:07:07 +0530

    update starPattern.js with a number pattern example

--- Arrays/starPattern.js
+++ Arrays/starPattern.js
@@ -3,7 +3,15 @@ let n = 6;
 for (let i = 0; i < n; i++) {
   let row = "";
   for (let j = 0; j < i + 1; j++) {
-    row = row + "*"
+    row = row + "*";
   }
   console.log(row);
+}
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < i + 1; j++) {
+    row = row + (j+1)
+  }
+  console.log(row)
 }
