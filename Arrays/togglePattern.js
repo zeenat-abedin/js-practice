a0a14c89e36622429e0ebefdb188403a7762e461
zeenat-abedin <zeenu.23@gmail.com>
2025-09-04 16:05:28 +0530

    update togglePattern.js

--- Arrays/togglePattern.js
+++ Arrays/togglePattern.js
@@ -24,3 +24,26 @@ for (let i = 0; i < n; i++) {
   }
   console.log(row);
 }
+
+// 1
+// 01
+// 010
+// 1010
+// 10101
+// 010101
+// 0101010
+
+let toggle = 1;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+    if (toggle == 1) {
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
+  }
+  console.log(row);
+}
