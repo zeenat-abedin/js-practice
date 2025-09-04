eebac4d139f09a16f6d3869d89726e26c8665174
zeenat-abedin <zeenu.23@gmail.com>
2025-09-04 12:17:00 +0530

    update reverseNumberPattern.js

--- Arrays/reverseNumberPattern.js
+++ Arrays/reverseNumberPattern.js
@@ -7,3 +7,14 @@ for (let i = 0; i < 5; i++) {
   }
   console.log(row);
 }
+
+for (let i = 0; i < n; i++) {
+    let row = ""
+    for (let j = 0; j < n-(i+1); j++) {
+       row = row + " "
+    }
+    for (let k = 0; k < i+1; k++) {
+       row = row + "*"
+    }
+    console.log(row)
+}
