da396e181b39c43cb9c33c067ac0e0f7f1558f84
zeenat-abedin <zeenu.23@gmail.com>
2025-09-10 18:15:34 +0530

    update formatting in removeElement.js

--- Arrays/removeElement.js
+++ Arrays/removeElement.js
@@ -6,14 +6,15 @@
 // 2 pointer approach
 
 function removeElement(nums, val) {
-    let x = 0; // first pointer is 0
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[x] = nums[i]
-            x = x + 1;
-        }
+  let x = 0; // first pointer is 0
+  for (let i = 0; i < nums.length; i++) {
+    //shift elements to left if it is not equal to val
+    if (nums[i] !== val) {
+      nums[x] = nums[i];
+      x = x + 1;
     }
-    return x;
+  }
+  return x;
 }
 
-console.log(removeElement([0, 0, 1, 1, 2, 2, 3, 3, 4], 3)) //8 since if we ignore val 3, then we have 8 elements in the array
+console.log(removeElement([0, 0, 1, 1, 2, 2, 3, 3, 4], 3)); //8 since if we ignore val 3, then we have 8 elements in the array
