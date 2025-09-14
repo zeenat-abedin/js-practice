6c16ec01922735f0ee1d8ca2aa191b1cb8d65b5c
zeenat-abedin <zeenu.23@gmail.com>
2025-09-14 11:47:45 +0530

    missingNumber: leetcode 268

---
+++ Arrays/missingNumber.js
@@ -0,0 +1,14 @@
+function missingNumber(nums) {
+  let n = nums.length;
+  let totalSum = (n * (n + 1)) / 2;
+
+  let partialSum = 0;
+  for (let i = 0; i < n; i++) {
+    partialSum = partialSum + nums[i];
+  }
+  return totalSum - partialSum;
+}
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
