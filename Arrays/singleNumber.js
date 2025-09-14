f98fb0ea75abb632525be6a5069bd7ab5e5cbb85
zeenat-abedin <zeenu.23@gmail.com>
2025-09-14 12:49:01 +0530

    singleNumber: leetcode 138

---
+++ Arrays/singleNumber.js
@@ -0,0 +1,26 @@
+// leetcode 138
+
+function singleNumber(nums) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]]++;
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] == 1) {
+      return nums[i];
+    }
+  }
+  return nums[i];
+}
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
+
+// time complexity -> o(n)
+// space complexity -> o(n) [since 0(n/2) is same as o(n)]
