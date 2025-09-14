9089ec64d9e6b547181cd25c2a1051dff3b77359
zeenat-abedin <zeenu.23@gmail.com>
2025-09-14 11:54:24 +0530

    update missingNumber.js

--- Arrays/missingNumber.js
+++ Arrays/missingNumber.js
@@ -14,3 +14,5 @@ function missingNumber(nums) {
 console.log(missingNumber([3, 0, 1]));
 console.log(missingNumber([0, 1]));
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+
+// time complexity -> o(n)
