fb7f44b2f10725380a372821733d3debfbf12074
zeenat-abedin <zeenu.23@gmail.com>
2025-09-19 21:37:00 +0530

    update maxProfit.js

--- Arrays/maxProfit.js
+++ Arrays/maxProfit.js
@@ -19,5 +19,5 @@ function maxProfit(prices) {
   return maxProfit;
 }
 
-console.log(maxProfit([7,1,5,3,6,4]))
-console.log(maxProfit([7,6,4,3,1]))
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
