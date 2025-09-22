32f10a370043e0b4a9b4765240106743597711f6
zeenat-abedin <zeenu.23@gmail.com>
2025-09-22 22:14:14 +0530

    update recursion.js

--- Arrays/recursion.js
+++ Arrays/recursion.js
@@ -1,10 +1,13 @@
+//Note- If you don't pass a base case, then it will run into an infinite loop
+
 function fun(num) {
-    if (num == 0) { //base case
-        return;
-    }
-    console.log(num)
-    num = num - 1;
-    fun(num)
+  if (num == 0) {
+    //base case
+    return;
+  }
+  console.log(num);
+  num = num - 1;
+  fun(num);
 }
 
-fun(10)
+fun(10);
