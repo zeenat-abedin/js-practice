ec399a6faeb093010aed377708662868bc8d882d
zeenat-abedin <zeenu.23@gmail.com>
2025-09-28 19:16:48 +0530

    update q11.js

--- Arrays/q11.js
+++ Arrays/q11.js
@@ -1,17 +1,16 @@
-const numbers = [1,2,3,4,5]
+const numbers = [1, 2, 3, 4, 5];
 
-const double = numbers.map(num => num * 2)
+const double = numbers.map((num) => num * 2);
 
 console.log(double);
 console.log(numbers); //doesn't modify the original array
 
 //higher order function example
 
-const numArr = [2,4,6,8,10,12,14]
+const numArr = [2, 4, 6, 8, 10, 12, 14];
 
 const half = numArr.map(function (num) {
-    return num / 2
-})
+  return num / 2;
+});
 
 console.log(half);
-
