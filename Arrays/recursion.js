0c270fda01771769f89511db674e33726055740f
zeenat-abedin <zeenu.23@gmail.com>
2025-09-29 12:29:02 +0530

    update recursion.js

--- Arrays/recursion.js
+++ Arrays/recursion.js
@@ -1,13 +1,9 @@
 //Note- If you don't pass a base case, then it will run into an infinite loop
 //Infinite recuursion leads to STACK OVERFLOW.
 
-function fun(num) {
-  if (num == 0) {  //base case
-    return;
-  }
-  console.log(num);
-  num = num - 1;
-  fun(num);
+function sum(n) {
+  if (n == 0) return 0; //base case
+  return n + sum(n-1)
 }
 
-fun(10);
+console.log(sum(6));
