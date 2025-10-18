5e8c3a0f14b0a339327b1b8227111533a4813440
zeenat-abedin <zeenu.23@gmail.com>
2025-10-18 10:54:46 +0530

    update eventBubblingVsCapturing.html
    loops practice
    
    test commit
    
    remove keyword
    
    check user
    
    Revert "remove keyword"
    
    This reverts commit 29a026748f58b85df3d0dd6346c0d4d3edac25c5.
    
    update

---
+++ Arrays/loops-practice.js
@@ -0,0 +1,20 @@
+//for of
+
+let str = "helloworld"
+
+for (let i of str){
+    console.log(i)
+}
+
+//for in
+
+let obj = {
+    name: "Zeenat",
+    age: 28,
+    city: "Bengaluru",
+    profession: "developer"
+}
+
+for (let keys in obj){
+    console.log(keys)
+}
