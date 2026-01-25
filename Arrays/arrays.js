1f1fbbda4be2665fa0aeccb38007192e7a4d10d0
zeenat-abedin <zeenu.23@gmail.com>
2026-01-25 23:29:53 +0530

    update arrays.js with arr for loop

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -4,3 +4,11 @@ const marks = [97, 45, 64, 73, 45]
 marks[0] = 22
 
 console.log('arrays:', marks);
+
+//for loop
+
+const heroes = ["ironman", "spiderman", "hulk", "shaktiman", "superman"]
+
+for (let i = 0; i < heroes.length; i++) {
+    console.log('hero is', heroes[i]);
+}
