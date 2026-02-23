96ab2cc960fb84fb1b017b93f4face758f9f467d
zeenat-abedin <zeenu.23@gmail.com>
2026-02-23 09:12:14 +0530

    countNegatives ex

---
+++ Arrays/loops.js
@@ -0,0 +1,11 @@
+function countNegatives(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+  return count
+}
+
+console.log('loops:', countNegatives([-2, 4, -1, -6, 3, 2, -7, -18]));
