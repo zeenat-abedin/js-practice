4df9ef90391215f0e9f63ba4148fb87b846864bb
zeenat-abedin <zeenu.23@gmail.com>
2026-02-23 13:59:24 +0530

    update loops.js with findLargest func

--- Arrays/loops.js
+++ Arrays/loops.js
@@ -9,3 +9,18 @@ function countNegatives(arr) {
 }
 
 console.log('countNegatives:', countNegatives([-2, 4, -1, -6, 3, 2, -7, -18]));
+
+//find largest
+
+function findLargest(arr) {
+    let largest = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+           largest = arr[i]
+        }
+    }
+    return largest
+    
+}
+
+console.log('findLargest:', findLargest([5, 0, 7, 10, 8, 17, 1]));
