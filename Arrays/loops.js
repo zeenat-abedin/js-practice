46bb880b8678be72cc7e44a7768113bcd8946f34
zeenat-abedin <zeenu.23@gmail.com>
2026-02-26 02:45:36 +0530

    update loops.js formatting

--- Arrays/loops.js
+++ Arrays/loops.js
@@ -5,7 +5,7 @@ function countNegatives(arr) {
       count++;
     }
   }
-  return count
+  return count;
 }
 
 console.log('countNegatives:', countNegatives([-2, 4, -1, -6, 3, 2, -7, -18]));
@@ -13,13 +13,13 @@ console.log('countNegatives:', countNegatives([-2, 4, -1, -6, 3, 2, -7, -18]));
 //find largest
 
 function findLargest(arr) {
-    let largest = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > largest) {
-           largest = arr[i]
-        }
+  let largest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
     }
-    return largest
+  }
+  return largest;
 }
 
 console.log('findLargest:', findLargest([5, 0, 7, 10, 8, 17, 1]));
@@ -27,14 +27,14 @@ console.log('findLargest:', findLargest([-9, -19, -3]));
 
 //find smallest
 
-function findSmallest(arr){
-    let smallest = Infinity; //when finding smallest, you can't put -Infinity
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < smallest) {
-           smallest = arr[i]
-        }
+function findSmallest(arr) {
+  let smallest = Infinity; //when finding smallest, you can't put -Infinity
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
     }
-    return smallest
+  }
+  return smallest;
 }
 
 console.log('findSmallest:', findSmallest([5, 0, 7, 10, 8, 17, 1]));
@@ -43,17 +43,18 @@ console.log('findSmallest:', findSmallest([-9, -19, -3]));
 //find second largest
 
 function secondLargest(arr) {
-    let firsLargest = -Infinity
-    let secondLargest = -Infinity
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > firsLargest) {
-            secondLargest = firsLargest
-            firsLargest = arr[i]
-        } else if (arr[i] > secondLargest){
-           secondLargest = arr[i]
-        }
+  let firsLargest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > firsLargest) {
+      secondLargest = firsLargest;
+      firsLargest = arr[i];
+    } else if (arr[i] > secondLargest) {
+      secondLargest = arr[i];
     }
-    return secondLargest
+  }
+  return secondLargest;
 }
 
-console.log('secondLargest:', secondLargest([ 4, 9, 0, 2, 8, 7, 1]));
+console.log('secondLargest:', secondLargest([4, 9, 0, 2, 8, 7, 1]));
