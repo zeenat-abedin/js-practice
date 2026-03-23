70d7bbad755738cfc2d72b54d629b07398bc96eb
zeenat-abedin <zeenu.23@gmail.com>
2026-03-23 12:02:28 +0530

    update recursion.js

--- Arrays/recursion.js
+++ Arrays/recursion.js
@@ -1,8 +1,12 @@
-//Note- If you don't pass a base case, then it will run
-//into an infinite loop
-//Infinite recuursion leads to STACK OVERFLOW.
+// Recursion - Function calls tself to solve smaller versions of the same problem
 
-//sum of first n natural numbers
+
+
+// Note- If you don't pass a base case, then it will run into an infinite loop
+
+// Infinite recuursion leads to STACK OVERFLOW.
+
+// sum of first n natural numbers
 
 function sum(n) {
   if (n == 0) return 0; //base case
@@ -12,3 +16,8 @@ function sum(n) {
 console.log(sum(6)); //(6+5+4+3+2+1 = 21)
 console.log(sum(5)); //(5+4+3+2+1 = 15)
 
+//sum of all numbers in an array
+
+function name(params) {
+  
+}
