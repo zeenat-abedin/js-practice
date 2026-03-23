d7e1f1fc8757eaa87f3c78ca555bc15b2924d744
zeenat-abedin <zeenu.23@gmail.com>
2026-03-23 19:41:46 +0530

    update recursion.js

--- Arrays/recursion.js
+++ Arrays/recursion.js
@@ -1,5 +1,9 @@
 // Recursion - Function calls tself to solve smaller versions of the same problem
 
+// Two parts of recursion - 
+// 1. Base case - Stop condition (when to stop calling itself)
+// 2. Recursive case - part where function calls
+
 // Note- If you don't pass a base case, then it will run into an infinite loop
 
 // Infinite recuursion leads to STACK OVERFLOW.
@@ -14,8 +18,13 @@ function sum(n) {
 console.log(sum(6)); //(6+5+4+3+2+1 = 21)
 console.log(sum(5)); //(5+4+3+2+1 = 15)
 
-//sum of all numbers in an array
+//sum of all elements in an array
 
-function name(params) {
-  
+let arr = [5, 3, 2, 0, 1]
+
+function sumOfAll(n) {
+  if (n == 0) return arr[0];
+  return arr[n] + sumOfAll(n - 1)
 }
+
+console.log('sum of all elements in an array:', sumOfAll(arr.length - 1));
