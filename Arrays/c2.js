2e9e143fc6138ce435434a00f67d33d84407be77
zeenat-abedin <zeenu.23@gmail.com>
2026-04-06 14:35:54 +0530

    update c2.js

--- Arrays/c2.js
+++ Arrays/c2.js
@@ -4,20 +4,30 @@ console.log('var1:', var1); //undefined
 let var2 = null;
 console.log('var2:', var2); //null
 
-
 // Write a function which helps to achieve multiply(a)(b) and returns product of a and b
 // Now create a curry function for the same
 
-function multiply(a){
-    return b => {
-        return a * b
-    }
+function multiply(a) {
+  return (b) => {
+    return a * b;
+  };
 }
 
-function curry(a){
-    return b => {
-        return a * b
-    }
+function curry(a) {
+  return (b) => {
+    return a * b;
+  };
 }
 
 console.log('multiply:', multiply(2)(3));
+
+//Write a function which gets an array and an element and returns an array with this element at the end.
+
+const numbers = [1, 2];
+
+const append = (arr, el) => {
+    arr.push(el)
+    return arr
+}
+
+console.log('c2:', append(numbers, 3));
