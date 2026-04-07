e955a68ee8cb6419252933f6c1872bad0d4524db
zeenat-abedin <zeenu.23@gmail.com>
2026-04-07 12:23:53 +0530

    update c2.js

--- Arrays/c2.js
+++ Arrays/c2.js
@@ -86,18 +86,17 @@ const arr = [3, 5, 1];
 const sortedArr = arr.sort((a, b) => b - a);
 console.log('originalArr', arr, 'sortedArr', sortedArr); // original arr got mutated...so sort mutates original arr
 
-//sort the object by author's lastname
+//sort array of objects by author's lastname
 
 const books = [
     {name: "Harry Potter", author: "Joanne Rowling" },
     {name: "Warcross", author: "Marie Lu" },
     {name: "The Hunger Games", author: "Suzanne Collins" }
 ]
-
 const sortedBooks = books.sort((book1, book2) => {
-    const authorLastName1 = book1.author.split("")
-    const authorLastName2 = book2.author.split("")
-    return authorLastName2 > authorLastName1 ? 1 : -1
+    const authorLastName1 = book1.author.split(" ")[1]
+    const authorLastName2 = book2.author.split(" ")[1]
+    return authorLastName1 < authorLastName2 ? -1 : 1
 })
 
 console.log('sortedBooks', sortedBooks);
