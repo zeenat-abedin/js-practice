180839c8966f9a40ebc613f4ed758053bd80d395
zeenat-abedin <zeenu.23@gmail.com>
2026-04-07 12:42:32 +0530

    update c2.js

--- Arrays/c2.js
+++ Arrays/c2.js
@@ -96,7 +96,7 @@ const books = [
 const sortedBooks = books.sort((book1, book2) => {
   const authorLastName1 = book1.author.split(' ')[1];
   const authorLastName2 = book2.author.split(' ')[1];
-  return authorLastName1 < authorLastName2 ? -1 : 1;
+  return authorLastName2 < authorLastName1 ? 1 : -1;
 });
 
 console.log('sortedBooks', sortedBooks);
