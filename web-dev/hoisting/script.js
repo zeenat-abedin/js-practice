e8bd694b7cab7c46c6c699a923c341b891801a46
zeenat-abedin <zeenu.23@gmail.com>
2026-04-12 19:34:08 +0530

    hoisting different examples

---
+++ web-dev/hoisting/script.js
@@ -0,0 +1,22 @@
+console.log(x); 
+getName()
+console.log(getName);
+
+var x = 10;
+
+function getName() {
+    console.log('Namaste Javascript');
+}
+
+// const getName = () => {
+//     console.log('Namaste Javascript');
+// }
+
+// Line 1 will log undefined
+// Line 2 will log Namaste Javascript
+// Line 3 will log the entire function since we are printing the getName method
+
+// If getName is arrow function
+
+// Line 1 will log undefined
+// Line 2 will log Uncaught ReferenceError: Cannot access 'getName' before initialization since arrow functions aren't hoisted
