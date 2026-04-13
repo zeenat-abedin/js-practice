7ebc1d46939cf4d31dc62680f309bedf4190e3f2
zeenat-abedin <zeenu.23@gmail.com>
2026-04-14 00:15:02 +0530

    update script.js

--- web-dev/hoisting/script.js
+++ web-dev/hoisting/script.js
@@ -1,11 +1,11 @@
-console.log(x); 
-getName()
+console.log(x);
+getName();
 console.log(getName);
 
 var x = 10;
 
 function getName() {
-    console.log('Namaste Javascript');
+  console.log('Namaste Javascript');
 }
 
 // const getName = () => {
