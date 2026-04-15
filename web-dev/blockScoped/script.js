bba1fc3329e61c1abb3208d4be71f9ba7289ed46
zeenat-abedin <zeenu.23@gmail.com>
2026-04-15 14:17:25 +0530

    update script.js

--- web-dev/blockScoped/script.js
+++ web-dev/blockScoped/script.js
@@ -1,6 +1,11 @@
+var a = 100;
 {
-    var a = 10;
-    let b = 20;
-    const c = 30;
-    console.log(a);
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
+ console.log(a); //will log 10 not 100 because of shadowing
+ console.log(b); //ReferenceError: b is not defined since b is block scoped
