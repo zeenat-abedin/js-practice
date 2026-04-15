29c8ab5903de6b72060afa17cad0ec1cc38e39fb
zeenat-abedin <zeenu.23@gmail.com>
2026-04-15 17:54:04 +0530

    update closure.js

--- web-dev/closure.js
+++ web-dev/closure.js
@@ -1,9 +1,61 @@
-function x() {
-    let a = 7;
-    function y() {
-        console.log(a)
+// function x() {
+//     let a = 7;
+//     function y() {
+//         console.log(a)
+//     }
+//     y()
+// }
+
+// x()
+
+// setTimeout + closures interview questions
+
+// function x() {
+//   var i = 1;
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 3000);
+//   console.log('Namaste Javascript');
+// }
+
+// x();
+
+// function x() {
+//   for (var i = 1; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   console.log('Namaste Javascript');
+// }
+
+// x();
+
+// function y() {
+//   for (let i = 1; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   console.log('Namaste Javascript');
+// }
+
+// y();
+
+// The reason why it works with let is because let is block scoped. So everytime/during each iteration, it create a fresh new copy of i
+
+// But we can make it work with var as well. Follow the below example
+
+function z() {
+  for (var i = 1; i <= 5; i++) {
+    function close(x) {
+      setTimeout(() => {
+        console.log(x);
+      }, i * 1000);
     }
-    y()
+    close(i);
+  }
+  console.log('Namaste Javascript');
 }
 
-x()
+z();
