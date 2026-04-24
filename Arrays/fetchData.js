b63374381073cb19b42ce693d987802b10473cd1
zeenat-abedin <zeenu.23@gmail.com>
2026-04-24 12:57:46 +0530

    update fetchData.js

--- Arrays/fetchData.js
+++ Arrays/fetchData.js
@@ -1,19 +1,19 @@
 // Write an example of fetching data with XMLHttpRequest.
 
-const xhr = new XMLHttpRequest()
+const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/gaearon/repos')
+xhr.open('GET', 'https://api.github.com/users/gaearon/repos');
 
-xhr.send()
+xhr.send();
 
 xhr.onload = function () {
-    if (xhr !== 200) {
-        console.log("Error" + xhr.status + xhr.statusText)
-    } else {
-        console.log("Success", xhr.response);
-    }
-}
+  if (xhr !== 200) {
+    console.log('Error' + xhr.status + xhr.statusText);
+  } else {
+    console.log('Success', xhr.response);
+  }
+};
 
 xhr.onerror = function () {
-    console.log("xhr request failed")
-}
+  console.log('xhr request failed');
+};
