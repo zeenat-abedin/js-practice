e2a86de80ac0b4ecbfe9c538ddffbc61930adfe9
zeenat-abedin <zeenu.23@gmail.com>
2026-04-27 00:29:43 +0530

    update cloneObject.js with shallow copy VS deep copy

--- Arrays/cloneObject.js
+++ Arrays/cloneObject.js
@@ -19,3 +19,26 @@ console.log(newObject)
 
 const clonedObjectJSON = JSON.parse(JSON.stringify(originalObject))
 console.log(clonedObjectJSON)
+
+// shallow copy VS deep copy (main difference)
+
+const  person = {
+  name: 'Zeenat',
+  age: 15,
+  address: {
+    city: 'Delhi',
+    country: 'India'
+  }
+};
+
+const shallowCopy = Object.assign({}, person)
+shallowCopy.address.city = 'Mumbai'
+
+console.log(shallowCopy.address.city)
+console.log(person.address.city) //original also got modified
+
+const deepCopy = JSON.parse(JSON.stringify(person))
+deepCopy.address.city = 'Bangalore'
+
+console.log(deepCopy.address.city)
+console.log(person.address.city) // original doesn't get modified
