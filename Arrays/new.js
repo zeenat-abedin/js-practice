059376b9dc9f7b6c9a19d49f0fbce3d483d953c0
zeenat-abedin <zeenu.23@gmail.com>
2026-04-30 10:19:13 +0530

    update new.js with output based question

--- Arrays/new.js
+++ Arrays/new.js
@@ -10,3 +10,23 @@ function outer(){
 
 let closureFunc = outer()
 closureFunc()
+
+// output order
+
+console.log('Start');
+
+setTimeout(function cbT(){
+  console.log('CB SetTimeout')
+}, 5000)
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function cbF(){
+  console.log('CB Todos')
+})
+  
+console.log('End');
+
+// Start
+// End
+// CB Todos (coz microtask has higher precision over callback queues)
+// CB SetTimeout
