7ed121afc34a4ba9309f2886c4ba3ce790719e0b
zeenat-abedin <zeenu.23@gmail.com>
2026-04-30 10:34:00 +0530

    update callApplyBind.js

--- Arrays/callApplyBind.js
+++ Arrays/callApplyBind.js
@@ -5,7 +5,7 @@ const dish = {
 }
 
 function cookBiryani(ing1, ing2) {
-    console.log(`Cooking biryani with ${ing1} and ${ing2}`)
+    console.log(`Cooking ${this.dish} with ${ing1} and ${ing2}`)
 }
 
 cookBiryani.call(dish, 'rice', 'chicken')
