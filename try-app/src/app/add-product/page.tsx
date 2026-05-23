1bd25bc53d3728ff17526d7b8d81aa9c1ff7ef81
zeenat-abedin <zeenu.23@gmail.com>
2026-05-23 18:42:41 +0530

    build(deps): update package-lock.json, package.json and page.tsx

--- try-app/src/app/add-product/page.tsx
+++ try-app/src/app/add-product/page.tsx
@@ -2,6 +2,34 @@ export default function AddProduct(){
     return (
         <div style={{margin: '4em'}}>
             <h1>Add product from here...</h1>
+            <form className="max-w-3xl p-12 m-auto border border-gray-300">
+            <div>
+                <div>
+                    <h1>Add Product</h1>
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="">Product Name</label>
+                        <div>
+                            <input type="text" name="name"/>
+                        </div>
+                    </div>
+                     <div>
+                        <label htmlFor="">Price</label>
+                        <div>
+                            <input type="text" name="price"/>
+                        </div>
+                    </div>
+                     <div>
+                        <label htmlFor="">Product Image</label>
+                        <div>
+                            <input type="file" name="image"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div></div>
+            </form>
         </div>
     )
 }
