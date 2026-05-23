edb26ee7ba27b84f06e00d233471a16478086f8a
zeenat-abedin <zeenu.23@gmail.com>
2026-05-23 12:28:24 +0530

    update layout.tsx and header.js

--- try-app/src/components/header.js
+++ try-app/src/components/header.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+    <>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/products">Products</Link>
-    </div>
+    </>
   );
 }
