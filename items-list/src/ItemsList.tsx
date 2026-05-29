c016750c532f83268ef27859e2c6dbae2d8077ae
zeenat-abedin <zeenu.23@gmail.com>
2026-05-29 19:16:09 +0530

    update App.tsx and ItemsList.tsx

--- items-list/src/ItemsList.tsx
+++ items-list/src/ItemsList.tsx
@@ -1,22 +1,22 @@
 type Item = {
-    id: number,
-    name: string
-}
+  id: number;
+  name: string;
+};
 
 const items: Item[] = [
-  { id: 1, name: "Apple" },
-  { id: 2, name: "Banana" },
-  { id: 3, name: "Orange" },
-  { id: 4, name: "Mango" },
-  { id: 5, name: "Grapes" },
+  { id: 1, name: 'Apple' },
+  { id: 2, name: 'Banana' },
+  { id: 3, name: 'Orange' },
+  { id: 4, name: 'Mango' },
+  { id: 5, name: 'Grapes' },
 ];
 
-export default function ItemsList(){
-    return (
-        <ul>
-            {items.map(el => 
-                <li>{el.name}</li>
-            )}
-        </ul>
-    )
+export default function ItemsList() {
+  return (
+    <ul>
+      {items.map((el) => (
+        <li>{el.name}</li>
+      ))}
+    </ul>
+  );
 }
