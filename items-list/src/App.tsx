19b1212bb4e83d1e9d1dd89108057659afec8f99
zeenat-abedin <zeenu.23@gmail.com>
2026-05-29 22:36:33 +0530

    update App.tsx and ItemsList.tsx

--- items-list/src/App.tsx
+++ items-list/src/App.tsx
@@ -1,23 +1,6 @@
 import './App.css'
 import ItemsList from './ItemsList'
 
-// Build a React (TSX preferred) component that
-// displays a list of items (e.g., fruits or products)
-// with the following features:
-// Requirements;
-// Display List
-// Show a list of items from a given array.
-// Each item should have a selectable control (checkbox/toggle).
-// Selection Logic
-// Users can select multiple items.
-// Display all selected items in a separate section.
-// Search Filter
-// Add a search input.
-// Filter the list in real-time as the user types.
-// Selection Limit
-// Allow selecting maximum 3 items only.
-// Disable further selections once the limit is reached.
-
 function App() {
   return (
     <>
--- items-list/src/ItemsList.tsx
+++ items-list/src/ItemsList.tsx
@@ -1,3 +1,20 @@
+// Build a React (TSX preferred) component that
+// displays a list of items (e.g., fruits or products)
+// with the following features:
+// Requirements;
+// Display List
+// Show a list of items from a given array.
+// Each item should have a selectable control (checkbox/toggle).
+// Selection Logic
+// Users can select multiple items.
+// Display all selected items in a separate section.
+// Search Filter
+// Add a search input.
+// Filter the list in real-time as the user types.
+// Selection Limit
+// Allow selecting maximum 3 items only.
+// Disable further selections once the limit is reached.
+
 type Item = {
   id: number;
   name: string;
