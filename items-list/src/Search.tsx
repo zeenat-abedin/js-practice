894077277f7cadff706966d7b6a68b1b5a8e898c
zeenat-abedin <zeenu.23@gmail.com>
2026-05-31 16:48:35 +0530

    update Search.tsx

--- items-list/src/Search.tsx
+++ items-list/src/Search.tsx
@@ -1,3 +1,26 @@
-export default function Search(){
-    return <div>This is search component</div>
+import { useMemo, useState } from 'react';
+
+export default function Search() {
+  const [list, setList] = useState(['apple', 'kiwi', 'orange']);
+  const [searchText, setSearchText] = useState('');
+
+  const filteredItems = useMemo(() => {
+    return list.filter((item) => item.toLowerCase().includes(searchText.toLowerCase()));
+  }, [list, searchText]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search item..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <ul>
+        {filteredItems.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
