6ff53ebc7e72b4233cea97bc02485d2b66f08b92
zeenat-abedin <zeenu.23@gmail.com>
2026-06-04 02:43:48 +0530

    update addTask handler

--- items-list/src/Todo.tsx
+++ items-list/src/Todo.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 
 export default function Todo() {
-  const [todos, setTodos] = useState(['learn advanced React in depth', 'practice basic DSA', 'brush up Javascript']);
+  const [todos, setTodos] = useState([
+    'learn advanced React in depth',
+    'practice basic DSA',
+    'brush up Javascript',
+  ]);
   const [text, setText] = useState('');
 
   function addTask() {
-    setTodos([...todos, text]);
+    if (!todos.includes(text)) {
+      setTodos([...todos, text]);
+    }
   }
 
   return (
     <>
       <div style={{ margin: 15 }}>
         <input type="text" onChange={(e) => setText(e.target.value)} />
-        <button onClick={addTask} >
-          Add Task
-        </button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       {todos.map((todo) => (
         <li key={todo}>{todo}</li>
