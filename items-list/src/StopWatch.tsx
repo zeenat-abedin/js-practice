e77aea127e228d59a7146d371862c75da173af6a
zeenat-abedin <zeenu.23@gmail.com>
2026-06-04 00:53:35 +0530

    update StopWatch.tsx with all the handlers

--- items-list/src/StopWatch.tsx
+++ items-list/src/StopWatch.tsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from 'react';
 export default function StopWatch() {
+  const [time, setTime] = useState(300);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval: number | undefined;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((time) => {
+          if (time > 0) {
+            return time - 1;
+          } else {
+            setIsActive(false);
+            return 0;
+          }
+        });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  function handleStart() {
+    setIsActive(true);
+  }
+
+  function handlePauseResume() {
+    setIsActive((prev) => !prev);
+  }
+
+  function handleStop() {
+    setIsActive(false);
+  }
+
+  function formatTime(time: number) {
+    const hours = Math.floor(time / 3600)
+      .toString()
+      .padStart(2, '0');
+    const mins = Math.floor((time % 3600) / 60)
+      .toString()
+      .padStart(2, '0');
+    const seconds = Math.floor(time % 60);
+    return `${hours} : ${mins} : ${seconds}`;
+  }
+
   return (
     <div>
       <h1>Stopwatch</h1>
-      <h1>Time: hh:mm:ss</h1>
-      <button>Start</button>
-      <button>Pause/Resume</button>
-      <button>Stop</button>
+      <h1>{formatTime(time)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handlePauseResume}>{isActive ? 'Pause' : 'Resume'}</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
