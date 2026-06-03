eeb82995cc726bad98452aa72cc5457480abc43b
zeenat-abedin <zeenu.23@gmail.com>
2026-06-04 02:32:47 +0530

    update Todo.tsx

--- items-list/src/Todo.tsx
+++ items-list/src/Todo.tsx
@@ -2,10 +2,20 @@ import { useState } from 'react';
 
 export default function Todo() {
   const [todos, setTodos] = useState(['learn react', 'practice basic DSA', 'brush up Javascript']);
+  const [text, setText] = useState('');
+
+  function addTask() {
+    setTodos([...todos, text]);
+  }
+
   return (
     <>
-      <input type="text" />
-      <button onClick={() => {}}>Add Task</button>
+      <div>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTask} style={{ paddingTop: 15 }}>
+          Add Task
+        </button>
+      </div>
       {todos.map((todo) => (
         <li key={todo}>{todo}</li>
       ))}
