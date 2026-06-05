49df9cac467cf80848d59a481e2a1f395bc61fa5
zeenat-abedin <zeenu.23@gmail.com>
2026-06-05 23:12:48 +0530

    handle onKeyUp for input field

--- items-list/src/Todo.tsx
+++ items-list/src/Todo.tsx
@@ -16,17 +16,28 @@ export default function Todo() {
   }
 
   function deleteTodo(todo: string) {
-    setTodos(todos.filter((curr) => curr !== todo))
+    setTodos(todos.filter((curr) => curr !== todo));
   }
 
   return (
     <>
       <div style={{ margin: 15 }}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              addTask();
+            }
+          }}
+        />
         <button onClick={addTask}>Add Task</button>
       </div>
       {todos.map((todo) => (
-        <li key={todo}>{todo} <button onClick={() => deleteTodo(todo)}>Delete</button></li>
+        <li key={todo}>
+          {todo} <button onClick={() => deleteTodo(todo)}>Delete</button>
+        </li>
       ))}
     </>
   );
