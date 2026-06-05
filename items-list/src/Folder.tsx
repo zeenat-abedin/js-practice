52061f74f465d84bb4e894819db2d774ebd12c20
zeenat-abedin <zeenu.23@gmail.com>
2026-06-06 00:22:10 +0530

    update Folder.tsx

--- items-list/src/Folder.tsx
+++ items-list/src/Folder.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 export default function Folder({ explorer }) {
+  const [expanded, setExpanded] = useState(false);
   return (
     <>
       <h3>Folder Structure/ Nested Accordion/ Nested Comments</h3>
-
+      <div onClick={() => setExpanded(!expanded)}>{explorer.name}</div>
+      <div>
+        {explorer.items.map((item: { name: string }) => (
+          <li>{item.name}</li>
+        ))}
+      </div>
     </>
   );
 }
