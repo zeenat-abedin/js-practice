56a60813ae2f98d338e71ef938bea7492f1cff02
zeenat-abedin <zeenu.23@gmail.com>
2026-06-10 16:56:50 +0530

    handle filteredProducts

--- items-list/src/Products.tsx
+++ items-list/src/Products.tsx
@@ -1,7 +1,66 @@
+import { useMemo, useState } from 'react';
+import { useProducts } from './hooks/useProducts';
+
 export default function Products() {
+  const { products, loading, error } = useProducts();
+  const [search, setSearch] = useState('');
+  const [category, setCategory] = useState('all');
+
+  const filteredProducts = useMemo(() => {
+    let result = [...products];
+    if (search.trim()) {
+      result = result.filter((product) =>
+        product.title.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    if (category !== 'all') {
+      result = result.filter((product) => product.category === category);
+    }
+
+    return result;
+  }, [category, products, search]);
+
+  const categories = useMemo(
+    () => ['all', ...new Set(products.map((product) => product.category))],
+    [products],
+  );
+
   return (
     <>
       <h1>Products Dashboard</h1>
+      <h4>{JSON.stringify(products)}</h4>
+      <input
+        type="text"
+        value={search}
+        placeholder="Search by name..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        {categories.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredProducts.map((item) => (
+            <tr key={item.id}>
+              <td>{item.title}</td>
+              <td>{item.category}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
--- items-list/src/hooks/useProducts.ts
+++ items-list/src/hooks/useProducts.ts
@@ -40,9 +40,9 @@ import { useEffect, useState } from 'react';
 
 interface Product {
   id: string;
-  name: string;
+  title: string;
   category: string;
-  image: string;
+  price: number;
 }
 
 export const useProducts = () => {
@@ -55,10 +55,10 @@ export const useProducts = () => {
       try {
         setLoading(true)
         const res = await fetch('https://fakestoreapi.com/products');
-        const data = await res.json()
         if (!res.ok) {
           throw new Error('Failed to fetch data')  
         }
+        const data = await res.json()
         setProducts(data)
       } catch (error) {
         setError(error instanceof Error ? error.message : '')
