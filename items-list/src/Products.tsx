40b1a15e5c632e434408ae860173ab9b666cc321
zeenat-abedin <zeenu.23@gmail.com>
2026-06-11 13:20:10 +0530

    remove unused import

--- items-list/src/Products.tsx
+++ items-list/src/Products.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useProducts } from './hooks/useProducts';
 
 export default function Products() {
