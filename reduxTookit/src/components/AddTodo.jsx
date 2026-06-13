eb249542238bcc5d17cb8c8736044505efd78656
zeenat-abedin <zeenu.23@gmail.com>
2026-06-13 16:23:00 +0530

    update AddTodo.jsx

--- reduxTookit/src/components/AddTodo.jsx
+++ reduxTookit/src/components/AddTodo.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 
 const AddTodo = () => {
   const [input, setInput] = useState('');
   const dispatch = useDispatch();
-  
-  return <div>AddTodo</div>;
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput('')
+  };
+
+  return <form onSubmit={addTodoHandler}></form>;
 };
 
 export default AddTodo;
