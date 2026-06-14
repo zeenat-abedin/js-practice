1db7876426016bd5c5b469da9290354445089e50
zeenat-abedin <zeenu.23@gmail.com>
2026-06-14 11:41:11 +0530

    update App.jsx, Todos.jsx, reduxTookit/src/index.css and main.jsx

--- reduxTookit/src/components/Todos.jsx
+++ reduxTookit/src/components/Todos.jsx
@@ -1,9 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeTodo } from '../features/todo/todoSlice';
 
 const Todos = () => {
   const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-  return <div>Todos</div>;
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}{' '}
+          <button
+            onClick={() => {
+              dispatch(removeTodo(todo.id));
+            }}
+          >
+            X
+          </button>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Todos;
