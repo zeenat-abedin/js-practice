8c71bdc04580ebd4f33d5f19f3c241e5fe4dc96d
zeenat-abedin <zeenu.23@gmail.com>
2026-06-25 19:03:22 +0530

    update nonPrimitives.js with array example

--- Arrays/nonPrimitives.js
+++ Arrays/nonPrimitives.js
@@ -7,5 +7,10 @@ user.lastName = 'Abedin'
 
 console.log(user)
 
+// array
+
+const anotherUser = ["Zeenat", true]
+console.log(anotherUser[0])
+
 let today = new Date()
 console.log(today.getDate())
