6389fd62687a84f64a12ea324b3f7aafccd8f209
zeenat-abedin <zeenu.23@gmail.com>
2026-06-26 07:25:30 +0530

    update nonPrimitives.js with soft copy example

--- Arrays/nonPrimitives.js
+++ Arrays/nonPrimitives.js
@@ -1,21 +1,33 @@
 const user = {
-    firstName: 'Zeenat',
-    isLoggedIn: true
-}
+  firstName: 'Zeenat',
+  isLoggedIn: true,
+};
 
-user.lastName = 'Abedin'
+user.lastName = 'Abedin';
 
-console.log(user)
+console.log(user);
 
 // array
 
-const anotherUser = ["Zeenat", true]
-console.log(anotherUser[0])
+const anotherUser = ['Zeenat', true];
+console.log(anotherUser[0]);
 
-let today = new Date()
-console.log(today.getDate())
+let today = new Date();
+console.log(today.getDate());
 
 //implicit conversion
 
 let isValue = true;
-console.log(isValue + 1) //2 --> since true == 1
+console.log(isValue + 1); //2 --> since true == 1
+
+/* You have an array named popularTeas containing green tea, oolong tea and
+chai. Create a soft copy of this array named softCopyTeas */
+
+const popularTeas = ["green tea", "oolong tea", "chai"]
+const softCopyTeas = popularTeas
+
+popularTeas.pop()
+
+console.log(popularTeas)
+console.log(softCopyTeas) //this will also get manipulated
+
