d48128c3d840c57d600e67ef0e599c1d7122b3ac
zeenat-abedin <zeenu.23@gmail.com>
2026-06-28 12:30:31 +0530

    while loop example

---
+++ Arrays/loopChallenges.js
@@ -0,0 +1,12 @@
+/* Write a while loop that calculates the sum of all numbers from 1 to 5
+sand stores the result in a variable named sum */
+
+let sum = 0;
+let i = 0;
+
+while (i <= 5) {
+  sum += i;
+  i++;
+}
+
+console.log(sum);
