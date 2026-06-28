12c5c4f97e21cd0dfc582d4efbbbec7f1ada04c9
zeenat-abedin <zeenu.23@gmail.com>
2026-06-28 13:29:01 +0530

    update loopChallenges.js with teaCollection example

--- Arrays/loopChallenges.js
+++ Arrays/loopChallenges.js
@@ -23,3 +23,19 @@ while (j > 0) {
 }
 
 console.log(countdown)
+
+/* Write a do while loop that prompts a user to enter
+their favourite tea type until they enter stop. 
+Store each tea type in array named teaCollection */
+
+let teaCollection = [];
+let tea = "";
+
+do {
+  tea = prompt(`Enter your favourite tea (type 'stop' to finish)`)  
+  if(tea === 'stop'){
+    teaCollection.push(tea)
+  }
+} while (tea !== 'stop');
+
+console.log(teaCollection)
