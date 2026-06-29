a81a2bb454c4f73de96daa28e0d9f76c3ef02e64
zeenat-abedin <zeenu.23@gmail.com>
2026-06-29 10:30:04 +0530

    update loopChallenges.js with for loop example

--- Arrays/loopChallenges.js
+++ Arrays/loopChallenges.js
@@ -1,4 +1,4 @@
-/* Write a while loop that calculates the sum of all numbers from 1 to 5
+/* 1. Write a while loop that calculates the sum of all numbers from 1 to 5
 sand stores the result in a variable named sum */
 
 let sum = 0;
@@ -11,7 +11,7 @@ while (i <= 5) {
 
 console.log(sum);
 
-/* Write a while loop that counts down from 5 to 1 and
+/* 2. Write a while loop that counts down from 5 to 1 and
 store the number in an array named countdown */
 
 let countdown = [];
@@ -24,23 +24,23 @@ while (j > 0) {
 
 console.log(countdown);
 
-/* Write a do while loop that prompts a user to enter
+/* 3. Write a do while loop that prompts a user to enter
 their favourite tea type until they enter stop. 
 Store each tea type in array named teaCollection */
 
-let teaCollection = [];
-let tea = '';
+// let teaCollection = [];
+// let tea = '';
 
-do {
-  tea = prompt(`Enter your favourite tea (type 'stop' to finish)`);
-  if (tea === 'stop') {
-    teaCollection.push(tea);
-  }
-} while (tea !== 'stop');
+// do {
+//   tea = prompt(`Enter your favourite tea (type 'stop' to finish)`);
+//   if (tea === 'stop') {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== 'stop');
 
-console.log(teaCollection);
+// console.log(teaCollection);
 
-/* Write a do while loop that adds numbers from 1 to 3
+/* 4. Write a do while loop that adds numbers from 1 to 3
  and stores the result in a variable named total. */
 
 let total = 0;
@@ -52,3 +52,17 @@ do {
 } while (k <= 3);
 
 console.log(total)
+
+/* 5. Write a for loop that multiplies each element in 
+the array [2, 4, 6] by 2 and stores the results in a 
+new array named multipliedNumbers */
+
+const numbers = [2, 4, 6]
+const multipliedNumbers = []
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i] * 2;
+    multipliedNumbers.push(element)
+}
+
+console.log(multipliedNumbers)
