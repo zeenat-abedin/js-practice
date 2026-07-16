029d50e18cc3b62c06228e722322bda584e9d09e
zeenat-abedin <zeenu.23@gmail.com>
2026-07-16 23:15:33 +0530

    update loopChallenges.js

--- Arrays/loopChallenges.js
+++ Arrays/loopChallenges.js
@@ -148,3 +148,16 @@ for (const element of teasArr) {
 }
 
 console.log(preferredTeas)
+
+/* 11. Use a for-in loop to loop through an object containing city populations. 
+Skip any city with a population below 3 million and store the rest in a new
+ object named 'largeCities'.*/
+
+let citiesPopulation = {
+  "London": 8900000,
+  "New York": 8400000,
+  "Paris": 2200000,
+  "Berlin": 3500000
+}
+
+console.log(Object.values(citiesPopulation))
