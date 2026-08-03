f3b3231d0dbe0d0ac0ccd3c52faa5032367f06f5
zeenat-abedin <zeenu.23@gmail.com>
2026-08-04 02:50:21 +0530

    update functionsChallenge with makeTea func.

---
+++ Arrays/functionsChallenge.js
@@ -0,0 +1,17 @@
+/*  1. Write a function named makeTea that takes
+one parameter typeOfTea and returns a string 
+like Making green tea when called with green tea. 
+Store the result in a variable named teaOrder */
+
+function makeTea(typeOfTea) {
+    return `Making ${typeOfTea}`
+}
+
+const teaOrder = makeTea("green tea")
+console.log(teaOrder)
+
+/* Create a function named orderTea that takes
+one parameter teaType. Inside this function, 
+create another function named confirmOrder 
+that returns a message like "Order confirmed for chai". 
+Call confirmOrder from within orderTea and return the result.*/
