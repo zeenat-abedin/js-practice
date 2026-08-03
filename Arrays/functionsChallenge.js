dc6ef7a1cc8598a44ba4253aff5a6ee5fd4ba625
zeenat-abedin <zeenu.23@gmail.com>
2026-08-04 04:27:29 +0530

    update functionsChallenge.js

--- Arrays/functionsChallenge.js
+++ Arrays/functionsChallenge.js
@@ -15,3 +15,13 @@ one parameter teaType. Inside this function,
 create another function named confirmOrder 
 that returns a message like "Order confirmed for chai". 
 Call confirmOrder from within orderTea and return the result.*/
+
+function orderTea(teaType) {
+    function confirmOrder() {
+        return `Order confirmed for ${teaType}`
+    }
+    return confirmOrder()
+}
+
+const orderConfirmation = orderTea("masala chai")
+console.log(orderConfirmation)
